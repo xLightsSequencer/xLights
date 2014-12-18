f6ad53b6620c6b12291825c523c30951d880e56d
djulien <djulien@thejuliens.net>
2014-12-17 20:50:49 -0800

    Add auto-deploy script for Mingw targets; an example script is provided (bin/deploy.bat), but it doesn't do anything so there will be no side effects to this change.

---
+++ bin/deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+rem - placeholder file to deploy to test/staging, or whatever else you want to do here
+rem - %1 = xLights executable name (relative path)
+echo "deploying %1 ..."
+rem - example:
+rem copy "%1"  "C:\MyShow"
