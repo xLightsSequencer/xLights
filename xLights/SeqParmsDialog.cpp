ccc93816133e372bb2ee160c0e21b46251b75ea8
dpitts <dpitts@ese-web.com>
2015-01-08 21:35:34 -0700

    Revert "Merge branch 'master' into v4"
    This reverts commit c3c09ff56bfbbbe1c394a04dc7f35a805ead8149, reversing
    changes made to 50747d02b8ae43de3d6268938c5980d43dd8b569.

--- xLights/SeqParmsDialog.cpp
+++ xLights/SeqParmsDialog.cpp
@@ -35,19 +35,16 @@ SeqParmsDialog::SeqParmsDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos
     StdDialogButtonSizer1->Realize();
     FlexGridSizer1->Add(StdDialogButtonSizer1, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
     StaticText_Filename = new wxStaticText(this, ID_STATICTEXT_FILENAME, _("Filename"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT_FILENAME"));
-    FlexGridSizer3->Add(StaticText_Filename, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    FlexGridSizer1->Add(FlexGridSizer3, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
+    FlexGridSizer1->Add(StaticText_Filename, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 10);
     FlexGridSizer2 = new wxFlexGridSizer(0, 2, 0, 0);
     StaticBoxSizer1 = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Models to Include"));
-    CheckListBox1 = new wxCheckListBox(this, ID_CHECKLISTBOX1, wxDefaultPosition, wxSize(325,158), 0, 0, wxLB_EXTENDED|wxLB_SORT, wxDefaultValidator, _T("ID_CHECKLISTBOX1"));
+    CheckListBox1 = new wxCheckListBox(this, ID_CHECKLISTBOX1, wxDefaultPosition, wxSize(325,158), 0, 0, wxLB_SORT, wxDefaultValidator, _T("ID_CHECKLISTBOX1"));
     StaticBoxSizer1->Add(CheckListBox1, 1, wxALL|wxEXPAND|wxALIGN_TOP|wxALIGN_CENTER_HORIZONTAL, 5);
     FlexGridSizer2->Add(StaticBoxSizer1, 1, wxALL|wxEXPAND|wxALIGN_TOP|wxALIGN_CENTER_HORIZONTAL, 5);
     FlexGridSizer1->Add(FlexGridSizer2, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     SetSizer(FlexGridSizer1);
     FlexGridSizer1->Fit(this);
     FlexGridSizer1->SetSizeHints(this);
-
-    Connect(ID_CHECKBOX_All,wxEVT_COMMAND_CHECKBOX_CLICKED,(wxObjectEventFunction)&SeqParmsDialog::OnCheckBox_AllClick);
     //*)
 }
 
@@ -55,13 +52,4 @@ SeqParmsDialog::~SeqParmsDialog()
 {
     //(*Destroy(SeqParmsDialog)
     //*)
-}
-
-void SeqParmsDialog::OnCheckBox_AllClick(wxCommandEvent& event)
-{
-//    wxArrayInt ignored;
-//    bool numsel = CheckListBox1->GetCheckedItems(ignored);
-//set all items to current value of All checkbox:
-    for (int i = 0; i < CheckListBox1->GetCount(); ++i)
-        CheckListBox1->Check(i, CheckBox_All->GetValue());
 }
