f58e7de0de679961a73767c2c17b77f9dc960ce7
Gil Jones <gil@threebuttes.com>
2015-02-01 01:17:19 -0700

    Add custom grid object files

---
+++ xLights/tmGridCell.cpp
@@ -0,0 +1,147 @@
+
+#include "tmGridCell.h"
+
+wxGridCellButtonRenderer::
+wxGridCellButtonRenderer(wxString label)
+{
+   m_strLabel = label;
+}
+
+wxGridCellButtonRenderer::
+~wxGridCellButtonRenderer(void)
+{
+}
+
+const long tmGrid::ID_GRID_RENDERER = wxNewId();
+
+void wxGridCellButtonRenderer::
+Draw(wxGrid &grid, wxGridCellAttr &attr, wxDC &dc, const wxRect &rect, int row, int col, bool isSelected)
+{
+   wxRendererNative::Get().DrawTitleBarBitmap(&grid, dc, rect, wxTITLEBAR_BUTTON_CLOSE, wxCONTROL_CURRENT);
+   //wxRendererNative::Get().DrawPushButton(&grid, dc, rect, wxCONTROL_CURRENT);
+   wxFont font = grid.GetFont();
+   dc.SetFont(font);
+   int x = rect.x + 10;
+   int y = rect.y + 1;
+   dc.DrawText(m_strLabel, x, y);
+}
+
+wxSize wxGridCellButtonRenderer::
+GetBestSize(wxGrid &grid, wxGridCellAttr &attr, wxDC &dc, int row, int col)
+{
+   wxString text = grid.GetCellValue(row, col);
+   dc.SetFont(attr.GetFont());
+   return dc.GetTextExtent(text);
+}
+
+wxGridCellRenderer *wxGridCellButtonRenderer::
+Clone() const
+{
+   return new wxGridCellButtonRenderer(m_strLabel);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+wxGridCellButtonEditor::
+wxGridCellButtonEditor(wxString label)
+{
+   m_strLabel = label;
+}
+
+wxGridCellButtonEditor::
+~wxGridCellButtonEditor(void)
+{
+}
+
+void wxGridCellButtonEditor::
+Create(wxWindow *parent, wxWindowID id, wxEvtHandler* pEvtHandler)
+{
+   m_pButton = new wxButton(parent, id, m_strLabel);
+   SetControl(m_pButton);
+   m_pButton->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(wxGridCellButtonEditor::OnButton));
+}
+
+void wxGridCellButtonEditor::
+OnButton(wxCommandEvent &evt)
+{
+
+   evt.Skip();
+}
+
+void wxGridCellButtonEditor::
+SetSize(const wxRect &rect)
+{
+   m_pButton->SetSize(rect.x, rect.y, rect.width+2, rect.height+2, wxSIZE_ALLOW_MINUS_ONE);
+}
+
+
+void wxGridCellButtonEditor::
+BeginEdit(int row, int col, wxGrid *pGrid)
+{
+   wxGridEvent evt(wxID_ANY, wxEVT_GRID_CELL_LEFT_CLICK, pGrid, row, col);
+   wxPostEvent(m_pButton, wxCommandEvent(wxEVT_COMMAND_BUTTON_CLICKED));
+}
+
+bool wxGridCellButtonEditor::
+EndEdit(int row, int col, const wxGrid *grid,
+        const wxString& oldval, wxString *newval)
+{
+   return false;
+}
+
+void wxGridCellButtonEditor::
+ApplyEdit(int row, int col, wxGrid* grid)
+{
+}
+
+void wxGridCellButtonEditor::
+Reset()
+{
+}
+
+wxString wxGridCellButtonEditor::
+GetValue() const
+{
+   return wxEmptyString;
+}
+
+wxGridCellEditor *wxGridCellButtonEditor::
+Clone() const
+{
+   return new wxGridCellButtonEditor(m_strLabel);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+BEGIN_EVENT_TABLE(tmGrid, wxGrid)
+   EVT_GRID_CELL_LEFT_CLICK(tmGrid::OnLeftClick)
+   EVT_GRID_CELL_CHANGED(tmGrid::OnCellChanged)
+END_EVENT_TABLE()
+
+tmGrid::
+tmGrid(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size, long style, const wxString &name)
+      : wxGrid(parent, id, pos, size, style, name)
+{
+}
+
+tmGrid::
+~tmGrid()
+{
+}
+
+void tmGrid::
+OnLeftClick(wxGridEvent &evt)
+{
+    SetGridCursor(evt.GetRow(), evt.GetCol());
+    //wxCommandEvent myevent(wxEVT_COMMAND_BUTTON_CLICKED, evt.GetRow());
+    wxCommandEvent myevent(EVT_DELETE_ROW, evt.GetRow());
+    wxPostEvent(this, myevent);
+    //evt.Skip();
+    //evt.StopPropagation();
+}
+
+void tmGrid::
+OnCellChanged(wxGridEvent &evt)
+{
+    //SetGridCursor(evt.GetRow(), evt.GetCol());
+    wxCommandEvent myevent(EVT_NAME_CHANGE, evt.GetRow());
+    wxPostEvent(this, myevent);
+}
---
+++ xLights/tmGridCell.h
@@ -0,0 +1,62 @@
+
+#include <wx/wx.h>
+#include <wx/grid.h>
+#include <wx/renderer.h>
+#include <wx/event.h>
+
+wxDECLARE_EVENT(EVT_DELETE_ROW, wxCommandEvent);
+wxDECLARE_EVENT(EVT_NAME_CHANGE, wxCommandEvent);
+
+class wxGridCellButtonRenderer : public wxGridCellRenderer
+{
+public:
+   wxGridCellButtonRenderer(wxString label);
+   virtual ~wxGridCellButtonRenderer(void);
+
+    virtual void Draw(wxGrid &grid, wxGridCellAttr &attr, wxDC &dc, const wxRect &rect, int row, int col, bool isSelected);
+    virtual wxSize GetBestSize(wxGrid &grid, wxGridCellAttr& attr, wxDC &dc, int row, int col);
+    virtual wxGridCellRenderer *Clone() const;
+private:
+    wxString m_strLabel;
+};
+
+
+class wxGridCellButtonEditor : public wxEvtHandler, public wxGridCellEditor
+{
+public:
+    wxGridCellButtonEditor(wxString label);
+   virtual ~wxGridCellButtonEditor(void);
+
+    virtual void Create(wxWindow *parent, wxWindowID id, wxEvtHandler* pEvtHandler);
+   void OnButton(wxCommandEvent &evt);
+
+    virtual void SetSize(const wxRect &rect);
+    virtual void BeginEdit(int row, int col, wxGrid *pGrid);
+    virtual bool EndEdit(int row, int col, const wxGrid *grid,
+                         const wxString& oldval, wxString *newval);
+    virtual void ApplyEdit(int row, int col, wxGrid* grid);
+    virtual void Reset();
+   virtual wxString GetValue() const;
+    virtual wxGridCellEditor *Clone() const;
+
+private:
+    wxString m_strLabel;
+   wxButton *m_pButton;
+
+    DECLARE_NO_COPY_CLASS(wxGridCellButtonEditor)
+};
+
+class tmGrid : public wxGrid
+{
+public:
+   tmGrid(wxWindow *parent, wxWindowID id, const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize,
+      long style = wxWANTS_CHARS, const wxString& name = wxPanelNameStr);
+   virtual ~tmGrid();
+
+   void OnLeftClick(wxGridEvent &evt);
+    void OnCellChanged(wxGridEvent &evt);
+
+   static const long ID_GRID_RENDERER;
+
+   DECLARE_EVENT_TABLE()
+};
