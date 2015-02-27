a94cace6b6d6061eae554da33597fd3e1e582f77
Gil Jones <gil@threebuttes.com>
2015-02-27 12:40:28 -0700

    Fix for saving more than one New Sequence per session.

--- xLights/SeqFileUtilities.cpp
+++ xLights/SeqFileUtilities.cpp
@@ -14,6 +14,7 @@ void xLightsFrame::NewSequence()
     }
 
     // clear everything to prepare for new sequence
+    xlightsFilename = "";
     mediaFilename.Clear();
     previewLoaded = false;
     previewPlaying = false;
