219bbb1ef3aadcf930119fe291b07672b447f3c3
sean <sean.meighan@oracle.com>
2015-03-09 12:15:09 -0600

    bumo version mar9 for release

--- xLights/xLightsMain.h
+++ xLights/xLightsMain.h
@@ -232,7 +232,7 @@ wxDECLARE_EVENT(EVT_IMPORT_TIMING, wxCommandEvent);
 
 static const wxString xlights_base_name = "XL4 ALPHA";
 static const wxString xlights_version_string = "4.0.0";
-static const wxString xlights_build_date = "Mar4";
+static const wxString xlights_build_date = "Mar9";
 
 static const wxString strSupportedFileTypes = "LOR Music Sequences (*.lms)|*.lms|LOR Animation Sequences (*.las)|*.las|HLS hlsIdata Sequences(*.hlsIdata)|*.hlsIdata|Vixen Sequences (*.vix)|*.vix|Glediator Record File (*.gled)|*.gled)|Lynx Conductor Sequences (*.seq)|*.seq|xLights Sequences(*.xseq)|*.xseq|xLights Imports(*.iseq)|*.iseq|Falcon Pi Player Sequences (*.fseq)|*.fseq";
 
