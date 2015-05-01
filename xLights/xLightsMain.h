bcce1969499be9629e48011c8c90a0651eb3b23f
sean <sean.meighan@oracle.com>
2015-05-01 08:53:58 -0600

    Checked in models xpm files to use for a model button bumped to 4.0.29 as pre release

--- xLights/xLightsMain.h
+++ xLights/xLightsMain.h
@@ -166,8 +166,8 @@ wxDECLARE_EVENT(EVT_IMPORT_TIMING, wxCommandEvent);
 
 
 static const wxString xlights_base_name       = "xLights 4 BETA";
-static const wxString xlights_version_string  = "4.0.28";
-static const wxString xlights_build_date      = "Apr29";
+static const wxString xlights_version_string  = "4.0.29";
+static const wxString xlights_build_date      = "May01 2015";
 
 static const wxString strSupportedFileTypes = "LOR Music Sequences (*.lms)|*.lms|LOR Animation Sequences (*.las)|*.las|HLS hlsIdata Sequences(*.hlsIdata)|*.hlsIdata|Vixen Sequences (*.vix)|*.vix|Glediator Record File (*.gled)|*.gled)|Lynx Conductor Sequences (*.seq)|*.seq|xLights Sequences(*.xseq)|*.xseq|xLights Imports(*.iseq)|*.iseq|Falcon Pi Player Sequences (*.fseq)|*.fseq";
 
