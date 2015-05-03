e6c0dc6a974e5cae0b7e0d4be9483b15ce74aa1c
Daniel Kulp <dkulp@apache.org>
2015-05-03 19:46:38 -0400

    If no colors are selected, don't crash.  Just display the effect icon

--- xLights/sequencer/EffectsGrid.cpp
+++ xLights/sequencer/EffectsGrid.cpp
@@ -936,12 +936,14 @@ bool EffectsGrid::DrawEffectBackground(const Effect *e, int x1, int y1, int x2, 
         break;
         case BitmapCache::RGB_EFFECTS_e::eff_GALAXY: {
             int head_duration = wxAtoi(e->GetSettings().Get("E_SLIDER_Galaxy_Duration", "20"));
+            int num_colors = e->GetPalette().size();
+            if (num_colors == 0) {
+                //no colors, don't draw a background and let the icon be drawn
+                return true;
+            }
             xlColor head_color = e->GetPalette()[0];
             int x_mid = (int)((float)(x2-x1) * (float)head_duration / 100.0) + x1;
             DrawGLUtils::DrawHBlendedRectangle(head_color, head_color, x1, y1+1, x_mid, y2-1);
-            int num_colors = e->GetPalette().size();
-            if( num_colors == 0 )
-                num_colors = 1;
             int color_length = (x2 - x_mid) / num_colors;
             for(int i = 0; i < num_colors; i++ )
             {
@@ -959,11 +961,13 @@ bool EffectsGrid::DrawEffectBackground(const Effect *e, int x1, int y1, int x2, 
         }
         case BitmapCache::RGB_EFFECTS_e::eff_FAN: {
             int head_duration = wxAtoi(e->GetSettings().Get("E_SLIDER_Fan_Duration", "50"));
+            int num_colors = e->GetPalette().size();
+            if (num_colors == 0) {
+                //no colors, don't draw a background and let the icon be drawn
+                return true;
+            }
             xlColor head_color = e->GetPalette()[0];
             int x_mid = (int)((float)(x2-x1) * (float)head_duration / 100.0) + x1;
-            int num_colors = e->GetPalette().size();
-            if( num_colors == 0 )
-                num_colors = 1;
             int head_length;
             int color_length;
             if( num_colors > 1 )
