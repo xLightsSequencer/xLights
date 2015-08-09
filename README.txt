232599b0657999fcf507f20f388961a623e2c3d5
sean <sean.meighan@oracle.com>
2015-08-08 21:42:15 -0600

    release 4.2.0

--- README.txt
+++ README.txt
@@ -7,6 +7,7 @@ Issue Tracker is found here: https://github.com/smeighan/xLights/issues
 
 XLIGHTS/NUTCRACKER RELEASE NOTES:
 ---------------------------------
+4.2.0  Aug 8, 2015
     -- bug(gil)  Fix channel map buttons disappearing when resizing dialog.  Fixes #313.
     -- bug(gil)  Prevent crash when doing Import Effects on xLights xml file.
     -- bug(gil)  Fix sparkles, brightness, and contrast text controls so that sliders update.  Fixes #320.
--- xLights/xLightsMain.h
+++ xLights/xLightsMain.h
@@ -170,8 +170,8 @@ wxDECLARE_EVENT(EVT_PROMOTE_EFFECTS, wxCommandEvent);
 
 
 static const wxString xlights_base_name       = "xLights 4 BETA";
-static const wxString xlights_version_string  = "4.1.13";
-static const wxString xlights_build_date      = "Jul 24, 2015";
+static const wxString xlights_version_string  = "4.2.0";
+static const wxString xlights_build_date      = "Aug 8, 2015";
 
 static const wxString strSupportedFileTypes = "LOR Music Sequences (*.lms)|*.lms|LOR Animation Sequences (*.las)|*.las|HLS hlsIdata Sequences(*.hlsIdata)|*.hlsIdata|Vixen Sequences (*.vix)|*.vix|Glediator Record File (*.gled)|*.gled)|Lynx Conductor Sequences (*.seq)|*.seq|xLights Sequences(*.xseq)|*.xseq|xLights Imports(*.iseq)|*.iseq|Falcon Pi Player Sequences (*.fseq)|*.fseq";
 static wxCriticalSection gs_xoutCriticalSection;
