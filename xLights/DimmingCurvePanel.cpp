c9d0a7b7ed468d141ae1b9ef6c30a6d5371d0e61
Gil Jones <gil@threebuttes.com>
2015-11-04 17:34:47 -0700

    Fix lag in dimming curve refresh.

--- xLights/DimmingCurvePanel.cpp
+++ xLights/DimmingCurvePanel.cpp
@@ -31,12 +31,13 @@ void DimmingCurvePanel::SetDimmingCurve(DimmingCurve *c, int ch) {
     curve = c;
     channel = ch;
     Refresh();
+    Update();
 }
 
 void DimmingCurvePanel::InitializeGLCanvas() {
     if(!IsShownOnScreen()) return;
     SetCurrentGLContext();
-    
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Black Background
     glDisable(GL_TEXTURE_2D);   // textures
     glEnable(GL_COLOR_MATERIAL);
@@ -51,29 +52,29 @@ void DimmingCurvePanel::render(wxPaintEvent& event) {
     if(!mIsInitialized) { InitializeGLCanvas(); }
     if(!IsShownOnScreen()) return;
     SetCurrentGLContext();
-    
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     wxPaintDC(this);
     if( mWindowResized )
     {
         prepare2DViewport(0,0,mWindowWidth, mWindowHeight);
     }
-    
+
     DrawGLUtils::DrawLine(xlLIGHT_GREY,255,2,2,mWindowWidth-2,2,0.5);
     DrawGLUtils::DrawLine(xlLIGHT_GREY,255,2,2,2,mWindowHeight-2,0.5);
     DrawGLUtils::DrawLine(xlLIGHT_GREY,255,mWindowWidth-2,2,mWindowWidth-2,mWindowHeight-2,0.5);
     DrawGLUtils::DrawLine(xlLIGHT_GREY,255,2,mWindowHeight-2,mWindowWidth-2,mWindowHeight-2,0.5);
-    
-    
+
+
     DrawGLUtils::DrawLine(xlLIGHT_GREY,255,mWindowWidth-2,2,2,mWindowHeight-2,0.5);
-    
+
     if (curve != nullptr) {
         DrawGLUtils::PreAlloc(256);
-        
+
         for (int x = 0; x < 255; x++) {
             xlColor c(x,x,x);
             curve->apply(c);
-            
+
             float xpos = float(x) * float(mWindowWidth - 4.0) / 255.0 + 2.0;
             float ypos = c.Red();
             switch (channel) {
@@ -87,14 +88,14 @@ void DimmingCurvePanel::render(wxPaintEvent& event) {
                     ypos = c.Blue();
                     break;
             }
-            
+
             ypos = mWindowHeight - 2 - ypos * float(mWindowHeight - 4.0) / 255.0;
-            
+
             DrawGLUtils::AddVertex(xpos, ypos, xlYELLOW);
         }
         DrawGLUtils::End(GL_LINE_STRIP, true);
     }
-    
+
     glFlush();
     SwapBuffers();
 }
