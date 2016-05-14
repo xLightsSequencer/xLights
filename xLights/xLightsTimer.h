2a5548951413606e7d57e950a90897c047cd31dd
Keith Westley <keith.westley@cba.com.au>
2016-05-15 08:59:07 +1000

    New timer. It should only impact the windows build. Mac and Linux should continue to use the inbuilt wxTimer. I have added a fudgefactor capability but it is set at zero right now.

---
+++ xLights/xLightsTimer.h
@@ -0,0 +1,47 @@
+#ifndef XLIGHTSTIMER_H
+#define XLIGHTSTIMER_H
+
+#include <wx/timer.h>
+
+#ifdef __WXMSW__
+#define XLUSETIMER
+#else
+#define xLightsTimer wxTimer
+#endif
+
+#ifdef XLUSETIMER
+
+#include <wx/thread.h>
+
+class xlTimerThread : public wxThread
+{
+public:
+    xlTimerThread(wxEvtHandler* pParent, int interval, bool oneshot, wxTimer* timer);
+    void Stop();
+    void SetFudgeFactor(int ff);
+private:
+    bool _oneshot;
+    bool _stop;
+    int _interval;
+    int _fudgefactor;
+    wxTimer* _timer;
+    virtual ExitCode Entry();
+protected:
+    wxEvtHandler* m_pParent;
+};
+
+class xLightsTimer :
+    public wxTimer
+{
+    xlTimerThread* _t;
+    int _time;
+    bool _oneshot;
+public:
+    xLightsTimer();
+    void Stop();
+    bool Start(int time = -1, bool oneShot = wxTIMER_CONTINUOUS);
+    int GetInterval();
+};
+
+#endif
+#endif
