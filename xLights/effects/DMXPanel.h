cf87f1b4083ec08ebbe014ce1b5282eaf7bd4e8a
Gil Jones <gil@threebuttes.com>
2016-06-24 17:35:37 -0700

    Fix files messed up when merging with changes that touched files I was working on.

--- xLights/effects/DMXPanel.h
+++ xLights/effects/DMXPanel.h
@@ -4,14 +4,16 @@
 //(*Headers(DMXPanel)
 #include <wx/panel.h>
 class wxTextCtrl;
-class wxChoice;
-class wxNotebook;
-class wxFlexGridSizer;
-class wxSlider;
 class wxNotebookEvent;
+class wxNotebook;
 class wxStaticText;
-class wxCheckBox;
+class wxSlider;
+class wxBitmapButton;
+class wxFlexGridSizer;
+class wxChoice;
 //*)
+
+#include "../ValueCurveButton.h"
 
 class DMXPanel: public wxPanel
 {
@@ -21,84 +23,90 @@ class DMXPanel: public wxPanel
 		virtual ~DMXPanel();
 
 		//(*Declarations(DMXPanel)
-		wxPanel* Panel21;
-		wxCheckBox* CheckBox_Use_Dmx_Ramps;
-		wxPanel* Panel20;
-		wxPanel* Panel18;
-		wxChoice* Choice_Num_Dmx_Channels;
-		wxPanel* Panel19;
+		ValueCurveButton* ValueCurve_DMX16;
 		wxNotebook* Notebook7;
+		ValueCurveButton* ValueCurve_DMX18;
+		ValueCurveButton* ValueCurve_DMX7;
+		ValueCurveButton* ValueCurve_DMX17;
+		ValueCurveButton* ValueCurve_DMX12;
+		ValueCurveButton* ValueCurve_DMX9;
+		ValueCurveButton* ValueCurve_DMX6;
+		ValueCurveButton* ValueCurve_DMX14;
+		wxChoice* Choice_Num_Dmx_Channels;
+		ValueCurveButton* ValueCurve_DMX13;
+		ValueCurveButton* ValueCurve_DMX4;
+		wxPanel* Panel18;
+		ValueCurveButton* ValueCurve_DMX2;
+		ValueCurveButton* ValueCurve_DMX1;
+		ValueCurveButton* ValueCurve_DMX3;
+		ValueCurveButton* ValueCurve_DMX15;
+		ValueCurveButton* ValueCurve_DMX10;
+		ValueCurveButton* ValueCurve_DMX8;
+		ValueCurveButton* ValueCurve_DMX11;
+		ValueCurveButton* ValueCurve_DMX5;
+		wxPanel* Panel19;
 		//*)
 
 	protected:
 
 		//(*Identifiers(DMXPanel)
 		static const long ID_SLIDER_DMX1;
+		static const long ID_VALUECURVE_DMX1;
 		static const long IDD_TEXTCTRL_DMX1;
 		static const long ID_SLIDER_DMX2;
+		static const long ID_VALUECURVE_DMX2;
 		static const long IDD_TEXTCTRL_DMX2;
 		static const long ID_SLIDER_DMX3;
+		static const long ID_VALUECURVE_DMX3;
 		static const long IDD_TEXTCTRL_DMX3;
 		static const long ID_SLIDER_DMX4;
+		static const long ID_VALUECURVE_DMX4;
 		static const long IDD_TEXTCTRL_DMX4;
 		static const long ID_SLIDER_DMX5;
+		static const long ID_VALUECURVE_DMX5;
 		static const long IDD_TEXTCTRL_DMX5;
 		static const long ID_SLIDER_DMX6;
+		static const long ID_VALUECURVE_DMX6;
 		static const long IDD_TEXTCTRL_DMX6;
 		static const long ID_SLIDER_DMX7;
+		static const long ID_VALUECURVE_DMX7;
 		static const long IDD_TEXTCTRL_DMX7;
 		static const long ID_SLIDER_DMX8;
+		static const long ID_VALUECURVE_DMX8;
 		static const long IDD_TEXTCTRL_DMX8;
 		static const long ID_SLIDER_DMX9;
+		static const long ID_VALUECURVE_DMX9;
 		static const long IDD_TEXTCTRL_DMX9;
 		static const long ID_CHOICE_Num_Dmx_Channels;
-		static const long ID_CHECKBOX_Use_Dmx_Ramps;
 		static const long ID_PANEL6;
 		static const long ID_SLIDER_DMX10;
+		static const long ID_VALUECURVE_DMX10;
 		static const long IDD_TEXTCTRL_DMX10;
 		static const long ID_SLIDER_DMX11;
+		static const long ID_VALUECURVE_DMX11;
 		static const long IDD_TEXTCTRL_DMX11;
 		static const long ID_SLIDER_DMX12;
+		static const long ID_VALUECURVE_DMX12;
 		static const long IDD_TEXTCTRL_DMX12;
 		static const long ID_SLIDER_DMX13;
+		static const long ID_VALUECURVE_DMX13;
 		static const long IDD_TEXTCTRL_DMX13;
 		static const long ID_SLIDER_DMX14;
+		static const long ID_VALUECURVE_DMX14;
 		static const long IDD_TEXTCTRL_DMX14;
 		static const long ID_SLIDER_DMX15;
+		static const long ID_VALUECURVE_DMX15;
 		static const long IDD_TEXTCTRL_DMX15;
+		static const long ID_SLIDER_DMX16;
+		static const long ID_VALUECURVE_DMX16;
+		static const long IDD_TEXTCTRL_DMX16;
+		static const long ID_SLIDER_DMX17;
+		static const long ID_VALUECURVE_DMX17;
+		static const long IDD_TEXTCTRL_DMX17;
+		static const long ID_SLIDER_DMX18;
+		static const long ID_VALUECURVE_DMX18;
+		static const long IDD_TEXTCTRL_DMX18;
 		static const long ID_PANEL28;
-		static const long ID_SLIDER_DMX1_Ramp;
-		static const long IDD_TEXTCTRL_DMX1_Ramp;
-		static const long ID_SLIDER_DMX2_Ramp;
-		static const long IDD_TEXTCTRL_DMX2_Ramp;
-		static const long ID_SLIDER_DMX3_Ramp;
-		static const long IDD_TEXTCTRL_DMX3_Ramp;
-		static const long ID_SLIDER_DMX4_Ramp;
-		static const long IDD_TEXTCTRL_DMX4_Ramp;
-		static const long ID_SLIDER_DMX5_Ramp;
-		static const long IDD_TEXTCTRL_DMX5_Ramp;
-		static const long ID_SLIDER_DMX6_Ramp;
-		static const long IDD_TEXTCTRL_DMX6_Ramp;
-		static const long ID_SLIDER_DMX7_Ramp;
-		static const long IDD_TEXTCTRL_DMX7_Ramp;
-		static const long ID_SLIDER_DMX8_Ramp;
-		static const long IDD_TEXTCTRL_DMX8_Ramp;
-		static const long ID_SLIDER_DMX9_Ramp;
-		static const long IDD_TEXTCTRL_DMX9_Ramp;
-		static const long ID_PANEL48;
-		static const long ID_SLIDER_DMX10_Ramp;
-		static const long IDD_TEXTCTRL_DMX10_Ramp;
-		static const long ID_SLIDER_DMX11_Ramp;
-		static const long IDD_TEXTCTRL_DMX11_Ramp;
-		static const long ID_SLIDER_DMX12_Ramp;
-		static const long IDD_TEXTCTRL_DMX12_Ramp;
-		static const long ID_SLIDER_DMX13_Ramp;
-		static const long IDD_TEXTCTRL_DMX13_Ramp;
-		static const long ID_SLIDER_DMX14_Ramp;
-		static const long IDD_TEXTCTRL_DMX14_Ramp;
-		static const long ID_SLIDER_DMX15_Ramp;
-		static const long IDD_TEXTCTRL_DMX15_Ramp;
-		static const long ID_PANEL49;
 		static const long ID_NOTEBOOK1;
 		//*)
 
