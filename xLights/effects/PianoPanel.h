821f3f21592e1d33d5ef55602c7a672d57360692
Keith Westley <keith.westley@cba.com.au>
2016-08-08 18:42:38 +1000

    Simplify piano to just use timing track. (reverted from commit 7cf637e9de3ad775c12f6646c29ac424704318a5)

--- xLights/effects/PianoPanel.h
+++ xLights/effects/PianoPanel.h
@@ -4,6 +4,7 @@
 //(*Headers(PianoPanel)
 #include <wx/panel.h>
 class wxSpinEvent;
+class wxFileDialog;
 class wxCheckBox;
 class wxTextCtrl;
 class wxStaticText;
@@ -11,10 +12,12 @@ class wxSlider;
 class wxBitmapButton;
 class wxFlexGridSizer;
 class wxSpinCtrl;
+class wxButton;
 class wxChoice;
 //*)
 
 #include <wx/progdlg.h>
+#include "../AudioManager.h"
 #include "../ValueCurveButton.h"
 
 class MidiFile;
@@ -25,37 +28,60 @@ public:
 
 		PianoPanel(wxWindow* parent);
 		virtual ~PianoPanel();
-        void SetTimingTrack(std::list<std::string> timingtracks) { _timingtracks = timingtracks; ValidateWindow(); };
+		void SetAudio(AudioManager* audio) { _media = audio; };
+        void SetTimingTrack(std::list<std::string> timingtracks) { _timingtracks = timingtracks; };
+        void TimingTrackValidateWindow();
 
 		//(*Declarations(PianoPanel)
 		wxStaticText* StaticText9;
 		wxTextCtrl* TextCtrl_Piano_XOffset;
+		wxBitmapButton* BitmapButton_Piano_MIDITrack_APPLYLAST;
+		wxBitmapButton* BitmapButton_Piano_Type;
+		wxTextCtrl* TextCtrl_Piano_File;
 		wxTextCtrl* TextCtrl_Piano_Scale;
 		wxStaticText* StaticText2;
 		wxSlider* Slider_Piano_Scale;
 		wxStaticText* StaticText_Piano_NumKeys;
 		wxCheckBox* CheckBox_Piano_ShowSharps;
+		wxStaticText* StaticText6;
 		wxBitmapButton* BitmapButton_Piano_StartMIDI;
+		wxSlider* Slider_Piano_MIDI_Speed;
 		wxStaticText* StaticText8;
 		wxSpinCtrl* SpinCtrl_Piano_StartMIDI;
 		wxBitmapButton* BitmapButton_Piano_EndMIDI;
+		wxTextCtrl* TextCtrl_Piano_MIDI_Speed;
+		wxChoice* Choice_Piano_Notes_Source;
+		wxBitmapButton* BitmapButton_Piano_MIDI_Speed;
 		wxChoice* Choice_Piano_MIDITrack_APPLYLAST;
 		wxStaticText* StaticText1;
+		wxFileDialog* FileDialog1;
+		wxBitmapButton* BitmapButton_Piano_MIDI_Start;
+		wxStaticText* StaticText3;
 		wxBitmapButton* BitmapButton_Piano_XOffset;
 		wxSpinCtrl* SpinCtrl_Piano_EndMIDI;
 		ValueCurveButton* BitmapButton_Piano_ScaleVC;
+		wxStaticText* StaticText5;
 		wxStaticText* StaticText7;
 		wxBitmapButton* BitmapButton_Piano_scale;
 		wxSlider* Slider_Piano_XOffset;
+		wxBitmapButton* BitmapButton_Piano_Notes_Source;
 		wxChoice* Choice_Piano_Type;
+		wxSlider* Slider_Piano_MIDI_Start;
+		wxStaticText* StaticText4;
+		wxButton* Button_Piano_File;
+		wxTextCtrl* TextCtrl_Piano_MIDI_Start;
 		wxBitmapButton* BitmapButton_Piano_ShowSharps;
 		//*)
 
 	protected:
 
 		//(*Identifiers(PianoPanel)
+		static const long ID_STATICTEXT3;
+		static const long ID_CHOICE_Piano_Notes_Source;
+		static const long ID_BITMAPBUTTON_Piano_Notes_Source;
 		static const long ID_STATICTEXT1;
 		static const long ID_CHOICE_Piano_Type;
+		static const long ID_BITMAPBUTTON_Piano_Type;
 		static const long ID_STATICTEXT2;
 		static const long ID_SPINCTRL_Piano_StartMIDI;
 		static const long ID_BITMAPBUTTON_Piano_StartMIDI;
@@ -64,8 +90,6 @@ public:
 		static const long ID_BITMAPBUTTON_Piano_EndMIDI;
 		static const long ID_CHECKBOX_Piano_ShowSharps;
 		static const long ID_BITMAPBUTTON_Piano_ShowSharps;
-		static const long ID_STATICTEXT8;
-		static const long ID_CHOICE_Piano_MIDITrack_APPLYLAST;
 		static const long ID_STATICTEXT7;
 		static const long ID_SLIDER_Piano_Scale;
 		static const long ID_VALUECURVE_Piano_Scale;
@@ -75,7 +99,22 @@ public:
 		static const long ID_SLIDER_Piano_XOffset;
 		static const long IDD_TEXTCTRL_Piano_XOffset;
 		static const long ID_BITMAPBUTTON_Piano_XOffset;
+		static const long ID_STATICTEXT4;
+		static const long ID_TEXTCTRL_Piano_File;
+		static const long ID_BUTTON_Piano_File;
+		static const long ID_STATICTEXT8;
+		static const long ID_CHOICE_Piano_MIDITrack_APPLYLAST;
+		static const long ID_BITMAPBUTTON_Piano_MIDITrack_APPLYLAST;
+		static const long ID_STATICTEXT5;
+		static const long ID_SLIDER_Piano_MIDI_Start;
+		static const long IDD_TEXTCTRL_Piano_MIDI_Start;
+		static const long ID_BITMAPBUTTON_Piano_MIDI_Start;
+		static const long ID_STATICTEXT6;
+		static const long ID_SLIDER_Piano_MIDI_Speed;
+		static const long IDD_TEXTCTRL_Piano_MIDI_Speed;
+		static const long ID_BITMAPBUTTON_Piano_MIDI_Speed;
 		//*)
+		AudioManager* _media;
         std::list<std::string> _timingtracks;
 
 	public:
@@ -88,8 +127,23 @@ public:
 		void UpdateLinkedTextCtrl(wxScrollEvent& event);
 		void UpdateLinkedSlider(wxCommandEvent& event);
 		void OnLockButtonClick(wxCommandEvent& event);
+		void OnPiano_StyleSelect(wxCommandEvent& event);
+		void OnSlider_Piano_NumKeysCmdScroll(wxScrollEvent& event);
+		void OnTextCtrl_Piano_FilenameText(wxCommandEvent& event);
+		void OnButton_Piano_FilenameClick(wxCommandEvent& event);
+		void OnSlider_Piano_KeyWidthCmdScroll(wxScrollEvent& event);
+		void OnButton_Piano_CueFilenameClick(wxCommandEvent& event);
+		void OnButton_Piano_MapFilenameClick(wxCommandEvent& event);
+		void OnButton_Piano_ShapeFilenameClick(wxCommandEvent& event);
+		void OnTextCtrl_Piano_CueFilenameText(wxCommandEvent& event);
+		void OnTextCtrl_Piano_MapFilenameText(wxCommandEvent& event);
+		void OnTextCtrl_Piano_ShapeFilenameText(wxCommandEvent& event);
+		void OnSlider_Piano_NumRowsCmdScroll(wxScrollEvent& event);
 		void OnSpinCtrl_Piano_StartMIDIChange(wxSpinEvent& event);
 		void OnSpinCtrl_Piano_EndMIDIChange(wxSpinEvent& event);
+		void OnChoice_Piano_Notes_SourceSelect(wxCommandEvent& event);
+		void OnTextCtrl_Piano_FileText(wxCommandEvent& event);
+		void OnButton_Piano_FileClick(wxCommandEvent& event);
         void UpdateLinkedTextCtrlVC(wxScrollEvent& event);
         void UpdateLinkedTextCtrlFloatVC(wxScrollEvent& event);
         void OnVCButtonClick(wxCommandEvent& event);
@@ -97,6 +151,9 @@ public:
         //*)
 
 		DECLARE_EVENT_TABLE()
+		void MIDIExtraValidateWindow();
+		void AudacityExtraValidateWindow();
+		bool MIDITrackContainsNotes(int track, MidiFile* midifile);
         void ValidateWindow();
 };
 
