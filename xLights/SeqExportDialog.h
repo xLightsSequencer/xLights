852528522498921558c55800ac813bfd65fcf188
Keith Westley <keith.westley@cba.com.au>
2016-08-09 22:28:30 +1000

    Remove video export for model groups

--- xLights/SeqExportDialog.h
+++ xLights/SeqExportDialog.h
@@ -6,22 +6,28 @@
 #include <wx/stattext.h>
 #include <wx/textctrl.h>
 #include <wx/choice.h>
+#include <wx/button.h>
 #include <wx/dialog.h>
 //*)
 
 class SeqExportDialog: public wxDialog
 {
+    void ValidateWindow();
+
 public:
 
     SeqExportDialog(wxWindow* parent,wxWindowID id=wxID_ANY,const wxPoint& pos=wxDefaultPosition,const wxSize& size=wxDefaultSize);
     virtual ~SeqExportDialog();
-    void ModelExportTypes();
+    void ModelExportTypes(bool isgroup);
 
     //(*Declarations(SeqExportDialog)
     wxTextCtrl* TextCtrlFilename;
     wxStaticText* StaticText1;
     wxStaticText* StaticText3;
+    wxButton* ButtonFilePick;
     wxChoice* ChoiceFormat;
+    wxButton* ButtonCancel;
+    wxButton* ButtonOk;
     //*)
 
 protected:
@@ -31,12 +37,19 @@ protected:
     static const long ID_CHOICE1;
     static const long ID_STATICTEXT3;
     static const long ID_TEXTCTRL2;
+    static const long ID_BUTTON1;
+    static const long ID_BUTTON2;
+    static const long ID_BUTTON3;
     //*)
 
 private:
 
     //(*Handlers(SeqExportDialog)
     void OnChoiceFormatSelect(wxCommandEvent& event);
+    void OnButtonFilePickClick(wxCommandEvent& event);
+    void OnButtonOkClick(wxCommandEvent& event);
+    void OnButtonCancelClick(wxCommandEvent& event);
+    void OnTextCtrlFilenameText(wxCommandEvent& event);
     //*)
 
     DECLARE_EVENT_TABLE()
