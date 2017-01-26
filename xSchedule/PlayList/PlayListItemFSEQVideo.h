5d0f9250effee302da188aa85e8bb2cfdb4188a4
Keith Westley <keith.westley@cba.com.au>
2017-01-27 09:55:01 +1100

    Create PlayListItemFSEQVideo.h

---
+++ xSchedule/PlayList/PlayListItemFSEQVideo.h
@@ -0,0 +1,102 @@
+#ifndef PLAYLISTITEMFSEQVIDEO_H
+#define PLAYLISTITEMFSEQVIDEO_H
+
+#include "PlayListItem.h"
+#include "../FSEQFile.h"
+#include <string>
+
+class wxXmlNode;
+class wxWindow;
+class AudioManager;
+class VideoReader;
+class PlayerWindow;
+struct AVFrame;
+
+class PlayListItemFSEQVideo : public PlayListItem
+{
+protected:
+
+    #pragma region Member Variables
+    APPLYMETHOD _applyMethod;
+    std::string _fseqFileName;
+    std::string _audioFile;
+    bool _overrideAudio;
+    FSEQFile* _fseqFile;
+    AudioManager* _audioManager;
+    size_t _durationMS;
+    bool _controlsTimingCache;
+    size_t _startChannel;
+    size_t _channels;
+    bool _fastStartAudio;
+	
+	std::string _videoFile;
+	wxPoint _origin;
+	wxSize _size;
+    VideoReader* _videoReader;
+	PlayerWindow* _window;
+#pragma endregion Member Variables
+
+    void LoadFiles();
+    void CloseFiles();
+    void FastSetDuration();
+    void LoadAudio();
+	wxImage CreateImageFromFrame(AVFrame* frame);
+
+public:
+
+    #pragma region Constructors and Destructors
+    PlayListItemFSEQVideo(wxXmlNode* node);
+    PlayListItemFSEQVideo();
+    virtual ~PlayListItemFSEQVideo();
+    virtual PlayListItem* Copy() const override;
+    #pragma endregion Constructors and Destructors
+
+    #pragma region Getters and Setters
+    std::string GetAudioFilename() const;
+    int GetBlendMode() const { return _applyMethod; }
+    void SetBlendMode(int blendMode) { if (_applyMethod != (APPLYMETHOD)blendMode) { _applyMethod = (APPLYMETHOD)blendMode; _changeCount++; } }
+    virtual size_t GetDurationMS() const override { return _delay + _durationMS; }
+    virtual std::string GetNameNoTime() const override;
+    std::string GetFSEQFileName() const { return _fseqFileName; }
+    std::string GetAudioFile() const { return _audioFile; }
+    bool GetOverrideAudio() const { return _overrideAudio; }
+    bool GetFastStartAudio() const { return _fastStartAudio; }
+    void SetFSEQFileName(const std::string& fseqFileName);
+    void SetAudioFile(const std::string& audioFile);
+    void SetOverrideAudio(bool overrideAudio);
+    void SetFastStartAudio(bool fastStartAudio);
+    virtual bool ControlsTiming() const override { return _controlsTimingCache || _audioManager != nullptr; }
+    virtual size_t GetPositionMS() const override;
+    virtual size_t GetFrameMS() const override { return _msPerFrame; }
+    virtual bool Done() const override { return GetPositionMS() >= GetDurationMS(); }
+    virtual std::string GetSyncItemName() const override { return GetFSEQFileName(); }
+    virtual std::string GetTitle() const override;
+    long GetStartChannel() const { return _startChannel; }
+    void SetStartChannel(long startChannel) { if (_startChannel != startChannel) { _startChannel = startChannel; _changeCount++; } }
+    long GetChannels() const { return _channels; }
+    void SetChannels(long channels) { if (_channels != channels) { _channels = channels; _changeCount++; } }
+	
+    void SetLocation(wxPoint pt, wxSize size) { if (_origin != pt || _size != size) { _origin = pt; _size = size; _changeCount++; } }
+    void SetVideoFile(const std::string& videoFile);
+    std::string GetVideoFile() const { return _videoFile; }
+    wxPoint GetPosition() const { return _origin; }
+    wxSize GetSize() const { return _size; }
+	#pragma endregion Getters and Setters
+
+    virtual wxXmlNode* Save() override;
+    void Load(wxXmlNode* node) override;
+
+    #pragma region Playing
+    virtual void Frame(wxByte* buffer, size_t size, size_t ms, size_t framems, bool outputframe) override;
+    virtual void Start() override;
+    virtual void Stop() override;
+    virtual void Restart() override;
+    virtual void Pause(bool pause) override;
+    virtual void Suspend(bool suspend) override;
+    #pragma endregion Playing
+
+#pragma region UI
+    virtual void Configure(wxNotebook* notebook) override;
+#pragma endregion UI
+};
+#endif
