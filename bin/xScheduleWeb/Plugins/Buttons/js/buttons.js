f8d8bf4d9d9e4026da1028d589e2bc39b2237f82
BillyFrostick <Billy.Frostick@gmail.com>
2017-03-19 06:58:25 -0400

    Buttons Plugin

---
+++ bin/xScheduleWeb/Plugins/Buttons/js/buttons.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+loadButtonsData()
+})
+function loadButtonsData(){
+  if (socket.readyState <= '1') {
+    socket.send('{"Type":"Query","Query":"GetButtons", "Reference":"populateButtons"}');
+  }else{
+    $.ajax({
+      url: '/xScheduleQueryQuery=' + command,
+      success: function(response) {
+        if (response.result == 'ok')
+          populateButtons(response);
+        if (response.result == 'failed')
+          notification('Failed: ' + response.message, 'danger', '0');
+      },
+      error: function(response) {
+        notification(response.result + ': ' + response.message, 'danger', '1');
+      }
+    });
+  }
+}
+function populateButtons(response){
+  console.log(response);
+  for (var i = 0; i < response.buttons.length; i++) {
+    var button = `<button onclick="runCommand('PressButton', 'id:`+response.buttons[i].id+`')" class="btn btn-default bigButtons">`+response.buttons[i].label+`</button>`;
+    $('#buttonContainer').append(button);
+  }
+}
--- bin/xScheduleWeb/js/sidebar.js
+++ bin/xScheduleWeb/js/sidebar.js
@@ -1,39 +1,42 @@
 function populateSideBar() {
   if (uiSettings != undefined) {
-    if (getPage(uiSettings, currentPage).values[0] == true) {
-      var sidebar1 = `
-      <div class="list-group">
-        <div class="list-group-item active main-color-bg"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Player Status </div>
-        <a href="" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>  Playlists </a>
-        <a href="" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>  Plugins </a>
-        <a href="" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>  Settings </a>
-      </div>
-      `;
-      $('#sideBar1').html(sidebar1);
-    }
-    if (getPage(uiSettings, currentPage).values[1] == true) {
-      var sidebar2 = `
-      <div class="panel panel-default">
-        <div class="panel-heading main-color-bg">
-          <h3 class="panel-title">Player Status</h3>
+    //disable custom sidebard for plugins
+    if(getQueryVariable("plugin") == false){
+      if (getPage(uiSettings, currentPage).values[0] == true) {
+        var sidebar1 = `
+        <div class="list-group">
+          <div class="list-group-item active main-color-bg"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Player Status </div>
+          <a href="" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>  Playlists </a>
+          <a href="" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>  Plugins </a>
+          <a href="" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>  Settings </a>
         </div>
-        <div class="panel-body">
-          <p>Coming Soon</p>
-        </div>
-      </div>
-      `;
-      $('#sideBar2').html(sidebar2);
-    }
-    //if no sidebar, remove it
-    if (getPage(uiSettings, currentPage).values[0] == false && getPage(uiSettings, currentPage).values[1] == false) {
-      $('#sidebarContainer').visibility = false;
-      $('#mainContainer').css("width", "100%");
-    }
-
-  } else {
-    sleep(100),
-      function() {
-        populateSideBar();
+        `;
+        $('#sideBar1').html(sidebar1);
       }
+      if (getPage(uiSettings, currentPage).values[1] == true) {
+        var sidebar2 = `
+        <div class="panel panel-default">
+          <div class="panel-heading main-color-bg">
+            <h3 class="panel-title">Player Status</h3>
+          </div>
+          <div class="panel-body">
+            <p>Coming Soon</p>
+          </div>
+        </div>
+        `;
+        $('#sideBar2').html(sidebar2);
+      }
+      //if no sidebar, remove it
+      if (getPage(uiSettings, currentPage).values[0] == false && getPage(uiSettings, currentPage).values[1] == false) {
+        $('#sidebarContainer').visibility = false;
+        $('#mainContainer').css("width", "100%");
+      }
+
+    } else {
+      sleep(100),
+        function() {
+          populateSideBar();
+        }
+    }
   }
 }
