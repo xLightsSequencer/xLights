2114c43c2a59bb1c97ea0080ffb3cb66f45c161e
Daniel Kulp <dkulp@apache.org>
2017-05-05 20:57:42 -0400

    Some checks to make sure we are setting stuff visible that don't exist

--- xLights/ViewsModelsPanel.cpp
+++ xLights/ViewsModelsPanel.cpp
@@ -438,18 +438,21 @@ void ViewsModelsPanel::PopulateModels(const std::string& selectModels)
         }
     }
 
-    if (topM + visibileM - 1< ListCtrlModels->GetItemCount())
-    {
-        ListCtrlModels->EnsureVisible(topM + visibileM - 1);
+    if (ListCtrlModels->GetItemCount() > 0) {
+        if (topM + visibileM - 1< ListCtrlModels->GetItemCount())
+        {
+            ListCtrlModels->EnsureVisible(topM + visibileM - 1);
+        }
+        ListCtrlModels->EnsureVisible(topM);
     }
-    ListCtrlModels->EnsureVisible(topM);
-
-    if (topN + visibileN - 1< ListCtrlNonModels->GetItemCount())
-    {
-        ListCtrlNonModels->EnsureVisible(topN + visibileN - 1);
+    if (ListCtrlNonModels->GetItemCount() > 0) {
+        if (topN + visibileN - 1< ListCtrlNonModels->GetItemCount())
+        {
+            ListCtrlNonModels->EnsureVisible(topN + visibileN - 1);
+        }
+        ListCtrlNonModels->EnsureVisible(topN);
     }
-    ListCtrlNonModels->EnsureVisible(topN);
-
+    
     if (selectModels != "")
     {
         wxArrayString models = wxSplit(selectModels, ',');
