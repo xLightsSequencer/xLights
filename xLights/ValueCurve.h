49dd9a99082dcaf92dc8f12a96445b96362a201c
Keith Westley <keith.westley@cba.com.au>
2017-06-11 18:27:44 +1000

    Fix value curve square curve not having all transitions immediate.

--- xLights/ValueCurve.h
+++ xLights/ValueCurve.h
@@ -5,13 +5,15 @@
 #include <string>
 #include <list>
 
+#define VC_X_POINTS 40.0
+
 class vcSortablePoint
 {
 public:
 
     static float Normalise(float v)
     {
-        return std::round(v * 40.0) / 40.0;
+        return std::round(v * VC_X_POINTS) / VC_X_POINTS;
     }
     float x;
     float y;
