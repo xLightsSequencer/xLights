f00515cbd3a6f718c7f178e3e41dcc5ca98a959e
Keith Westley <keith.westley@cba.com.au>
2017-09-04 11:03:04 +1000

    Remove redundant file.

--- bin/deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem - placeholder file to deploy to test/staging, or whatever else you want to do here
-rem - %1 = xLights executable name (relative path)
-echo "deploying %1 ..."
-rem - example:
-rem copy "%1"  "C:\MyShow"
