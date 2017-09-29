78c0fbe93ca64727616924ca88d2684b3414a6dc
sean <sean@mighan.nt>
2017-09-29 13:28:10 -0600

    2017.30, Production Release

--- xLights/xLightsVersion.h
+++ xLights/xLightsVersion.h
@@ -3,9 +3,9 @@
 
 // These have been moved here so they can be used in xSchedule as well.
 
-static const wxString xlights_qualifier       = "BETA"; // " BETA";
-static const wxString xlights_version_string  = "2017.29";
-static const wxString xlights_build_date      = "Sep 19, 2017";
+static const wxString xlights_qualifier       = " PROD"; // " BETA,ALPHA,PROD";
+static const wxString xlights_version_string  = "2017.30";
+static const wxString xlights_build_date      = "Sep 29, 2017";
 
 wxString GetBitness();
 
