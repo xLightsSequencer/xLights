133d78ca836d005548078989f0148f4b1b923d18
Daniel Kulp <dkulp@apache.org>
2017-11-13 14:47:20 -0500

    Update Mac build

--- xLights/xLightsVersion.h
+++ xLights/xLightsVersion.h
@@ -4,7 +4,7 @@
 // These have been moved here so they can be used in xSchedule as well.
 
 static const wxString xlights_qualifier       = " BETA"; // " BETA,ALPHA,PROD";
-static const wxString xlights_version_string  = "2017.35";
+static const wxString xlights_version_string  = "2017.36";
 static const wxString xlights_build_date      = "Nov 05, 2017";
 
 wxString GetBitness();
