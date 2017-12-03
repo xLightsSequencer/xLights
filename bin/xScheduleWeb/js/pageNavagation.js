a19a61e68e1c0c1c6e0f478b7861285f4979a114
Rich Danby <cinoan@gmail.com>
2017-12-03 19:20:14 +0000

    Fix URL parameter decoding (#1000)

--- bin/xScheduleWeb/js/pageNavagation.js
+++ bin/xScheduleWeb/js/pageNavagation.js
@@ -71,7 +71,7 @@ function getQueryVariable(variable) {
   for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split("=");
     if (pair[0] == variable) {
-      return pair[1];
+      return decodeURIComponent(pair[1]);
     }
   }
   return (false);
