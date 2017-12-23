a66e3d745eb473691780e163af4817e1f6d6a0b3
Keith Westley <keith.westley@cba.com.au>
2017-12-23 11:54:10 +1100

    Fix #1040

--- xSchedule/MatrixDialog.cpp
+++ xSchedule/MatrixDialog.cpp
@@ -75,12 +75,12 @@ MatrixDialog::MatrixDialog(wxWindow* parent, OutputManager* outputManager, std::
 	FlexGridSizer1->Add(SpinCtrl_Strings, 1, wxALL|wxEXPAND, 5);
 	StaticText4 = new wxStaticText(this, ID_STATICTEXT4, _("String Length:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT4"));
 	FlexGridSizer1->Add(StaticText4, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
-	SpinCtrl_StringLength = new wxSpinCtrl(this, ID_SPINCTRL2, _T("150"), wxDefaultPosition, wxDefaultSize, 0, 1, 1024, 150, _T("ID_SPINCTRL2"));
+	SpinCtrl_StringLength = new wxSpinCtrl(this, ID_SPINCTRL2, _T("150"), wxDefaultPosition, wxDefaultSize, 0, 1, 65535, 150, _T("ID_SPINCTRL2"));
 	SpinCtrl_StringLength->SetValue(_T("150"));
 	FlexGridSizer1->Add(SpinCtrl_StringLength, 1, wxALL|wxEXPAND, 5);
 	StaticText2 = new wxStaticText(this, ID_STATICTEXT2, _("Strands Per String:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
 	FlexGridSizer1->Add(StaticText2, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
-	SpinCtrl_StrandsPerString = new wxSpinCtrl(this, ID_SPINCTRL3, _T("1"), wxDefaultPosition, wxDefaultSize, 0, 0, 64, 1, _T("ID_SPINCTRL3"));
+	SpinCtrl_StrandsPerString = new wxSpinCtrl(this, ID_SPINCTRL3, _T("1"), wxDefaultPosition, wxDefaultSize, 0, 0, 8192, 1, _T("ID_SPINCTRL3"));
 	SpinCtrl_StrandsPerString->SetValue(_T("1"));
 	FlexGridSizer1->Add(SpinCtrl_StrandsPerString, 1, wxALL|wxEXPAND, 5);
 	StaticText3 = new wxStaticText(this, ID_STATICTEXT3, _("Start Channel:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT3"));
