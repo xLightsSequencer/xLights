c734020025d4f84e37068114af882df237f99bb6
Scott <scooter_seh@yahoo.com>
2018-03-21 22:17:02 -0400

    Added Search Box to Effect Preset Dialog

--- xLights/EffectTreeDialog.h
+++ xLights/EffectTreeDialog.h
@@ -5,6 +5,7 @@
 #include <wx/button.h>
 #include <wx/dialog.h>
 #include <wx/sizer.h>
+#include <wx/textctrl.h>
 #include <wx/treectrl.h>
 //*)
 #include <wx/xml/xml.h>
@@ -25,6 +26,7 @@ class EffectTreeDialog : public wxDialog
 		virtual ~EffectTreeDialog();
 
 		//(*Declarations(EffectTreeDialog)
+		wxButton* Button1;
 		wxButton* btAddGroup;
 		wxButton* btApply;
 		wxButton* btDelete;
@@ -33,6 +35,7 @@ class EffectTreeDialog : public wxDialog
 		wxButton* btNewPreset;
 		wxButton* btRename;
 		wxButton* btUpdate;
+		wxTextCtrl* TextCtrl1;
 		wxTreeCtrl* TreeCtrl1;
 		//*)
         wxTreeItemId treeRootID;
@@ -50,6 +53,8 @@ class EffectTreeDialog : public wxDialog
 		static const long ID_BUTTON4;
 		static const long ID_BUTTON5;
 		static const long ID_BUTTON8;
+		static const long ID_TEXTCTRL_SEARCH;
+		static const long ID_BUTTON_SEARCH;
 		//*)
 
 	private:
@@ -68,6 +73,8 @@ class EffectTreeDialog : public wxDialog
 		void OnbtImportClick(wxCommandEvent& event);
 		void OnbtExportClick(wxCommandEvent& event);
 		void OnTreeCtrl1SelectionChanged(wxTreeEvent& event);
+		void OnButton1Click(wxCommandEvent& event);
+		void OnTextCtrl1TextEnter(wxCommandEvent& event);
 		//*)
 
         xLightsFrame* xLightParent;
@@ -80,6 +87,10 @@ class EffectTreeDialog : public wxDialog
         void AddEffect(wxXmlNode* ele, wxTreeItemId curGroupID);
         void AddGroup(wxXmlNode* ele, wxTreeItemId curGroupID);
         void EffectsFileDirty();
+
+        wxTreeItemId findTreeItem(wxTreeCtrl* pTreeCtrl, const wxTreeItemId& root, const wxTreeItemId& startID, const wxString& text, bool &startfound);
+
+        void SearchForText();
 
 		DECLARE_EVENT_TABLE()
 
