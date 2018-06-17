ba199129ea4b6dd2f53c15b951b7352210c346b4
Keith Westley <keith.westley@cba.com.au>
2018-06-17 10:11:46 +1000

    2018.16

--- xLights/xLightsVersion.h
+++ xLights/xLightsVersion.h
@@ -4,8 +4,8 @@
 // These have been moved here so they can be used in xSchedule as well.
 
 static const wxString xlights_qualifier       = " "; // " BETA,ALPHA,PROD";
-static const wxString xlights_version_string  = "2018.15";
-static const wxString xlights_build_date      = "June 10, 2018";
+static const wxString xlights_version_string  = "2018.16";
+static const wxString xlights_build_date      = "June 17, 2018";
 
 wxString GetBitness();
 
