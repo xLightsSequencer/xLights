d6349cb154b556e8995eec3c4ef7a538dd380033
Keith Westley <keith.westley@cba.com.au>
2018-07-06 23:19:33 +1000

    Fix spirals rotation value curve incorrect.

--- xLights/effects/SpiralsEffect.cpp
+++ xLights/effects/SpiralsEffect.cpp
@@ -57,6 +57,11 @@ void SpiralsEffect::Render(Effect *effect, SettingsMap &SettingsMap, RenderBuffe
     int PaletteRepeat = GetValueCurveInt("Spirals_Count", 1, SettingsMap, offset, SPIRALS_COUNT_MIN, SPIRALS_COUNT_MAX);
     float Movement = GetValueCurveDouble("Spirals_Movement", 1.0, SettingsMap, offset, SPIRALS_MOVEMENT_MIN, SPIRALS_MOVEMENT_MAX, SPIRALS_MOVEMENT_DIVISOR);
     float Rotation = GetValueCurveDouble("Spirals_Rotation", 0.0, SettingsMap, offset, SPIRALS_ROTATION_MIN, SPIRALS_ROTATION_MAX, SPIRALS_ROTATION_DIVISOR);
+    // This is because spirals uses the slider while most others use the TextCtrl
+    if (SettingsMap.Contains("VALUECURVE_Spirals_Rotation") && wxString(SettingsMap["VALUECURVE_Spirals_Rotation"]).Contains("Active=TRUE"))
+    {
+        Rotation *= 10;
+    }
     int Thickness = GetValueCurveInt("Spirals_Thickness", 0, SettingsMap, offset, SPIRALS_THICKNESS_MIN, SPIRALS_THICKNESS_MAX);
     bool Blend = SettingsMap.GetBool("CHECKBOX_Spirals_Blend");
     bool Show3D = SettingsMap.GetBool("CHECKBOX_Spirals_3D");
