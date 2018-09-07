342fa53dea2ea596bf5efd969fb091c5262d6429
Keith Westley <keith.westley@cba.com.au>
2018-09-07 17:13:16 +1000

    Change the way falcon model/version is detected to make the upload more reliable.

--- xLights/TabSetup.cpp
+++ xLights/TabSetup.cpp
@@ -1776,6 +1776,10 @@ void xLightsFrame::UploadFalconInput()
                 SetStatusText("Falcon Input Upload Failed.");
             }
         }
+        else
+        {
+            SetStatusText("Falcon Input Upload Failed.");
+        }
         SetCursor(wxCURSOR_ARROW);
     }
 }
