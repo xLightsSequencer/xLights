d33a139592179e15a059f4a19335126dffb8abad
Keith Westley <keith.westley@cba.com.au>
2018-10-28 20:24:09 +1100

    Some tweaks to ftp

--- xLights/controllers/SimpleFTP.h
+++ xLights/controllers/SimpleFTP.h
@@ -13,7 +13,7 @@ public:
     SimpleFTP();
     SimpleFTP(std::string ip, std::string user, std::string password);
     bool Connect(std::string ip, std::string user, std::string password);
-    bool IsConnected();
+    bool IsConnected() const;
     bool UploadFile(std::string file, std::string folder, std::string newfilename, bool backup, bool binary, wxWindow* parent);
     bool GetFile(std::string targetfile, std::string folder, std::string file, bool binary, wxWindow* parent);
     ~SimpleFTP();
