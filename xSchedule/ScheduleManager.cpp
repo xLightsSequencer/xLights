e75964acb2a3e8c8539f4d3ece0c439b62df6f7b
Keith Westley <keith.westley@cba.com.au>
2018-12-13 11:30:23 +1100

    Update ScheduleManager.cpp

--- xSchedule/ScheduleManager.cpp
+++ xSchedule/ScheduleManager.cpp
@@ -270,7 +270,8 @@ int ScheduleManager::Sync(const std::string& filename, long ms)
 int ScheduleManager::DoSync(const std::string& filename, long ms)
 {
     static log4cpp::Category &logger_base = log4cpp::Category::getInstance(std::string("log_base"));
-
+logger_base.debug("DoSync Enter");
+	
     PlayList* pl = GetRunningPlayList();
     PlayListStep* pls = nullptr;
 
@@ -404,14 +405,17 @@ int ScheduleManager::DoSync(const std::string& filename, long ms)
     if (pls != nullptr)
     {
         _listenerManager->SetFrameMS(pls->GetFrameMS());
+logger_base.debug("DoSync Leave");
         return pls->GetFrameMS();
     }
     if (pl != nullptr)
     {
         _listenerManager->SetFrameMS(pl->GetFrameMS());
+logger_base.debug("DoSync Leave");
         return pl->GetFrameMS();
     }
     _listenerManager->SetFrameMS(50);
+logger_base.debug("DoSync Leave");
     return 50;
 }
 
