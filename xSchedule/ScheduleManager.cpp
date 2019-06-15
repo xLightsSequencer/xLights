656dbe0f155d99180d36a95ba943335ed327c6fa
Keith Westley <keithsw1111@hotmail.com>
2019-06-15 19:45:11 +1000

    Clear the buffer when just playing event playlists

--- xSchedule/ScheduleManager.cpp
+++ xSchedule/ScheduleManager.cpp
@@ -1104,6 +1104,7 @@ int ScheduleManager::Frame(bool outputframe, xScheduleFrame* frame)
                 {
                     if (outputframe)
                     {
+                        memset(_buffer, 0x00, totalChannels); // clear out any prior frame data
                         _outputManager->StartFrame(0);
                         _outputManager->AllOff(false);
                     }
