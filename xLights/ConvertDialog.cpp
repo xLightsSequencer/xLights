3dbd894c4fa16c77a664cef9d45e946d8efbce2f
Keith Westley <keithsw1111@hotmail.com>
2019-06-17 19:36:00 +1000

    Try again

--- xLights/ConvertDialog.cpp
+++ xLights/ConvertDialog.cpp
@@ -19,6 +19,9 @@
 #include "outputs/OutputManager.h"
 
 // xml
+#ifdef LINUX
+#define _strdup strdup
+#endif
 #include "../include/spxml-0.5/spxmlparser.hpp"
 #include "../include/spxml-0.5/spxmlevent.hpp"
 #include "../include/spxml-0.5/spxmlparser.cpp"
