5622d3922a3d6e239dd3d0f63dd28d3d045b53ef
Keith Westley <keithsw1111@hotmail.com>
2019-11-26 20:53:45 +1100

    Let xLights crash in debugger

--- xLights/xLightsApp.cpp
+++ xLights/xLightsApp.cpp
@@ -561,7 +561,9 @@ bool xLightsApp::OnInit()
 #endif
 
 #if wxUSE_ON_FATAL_EXCEPTION
-    wxHandleFatalExceptions();
+    #ifndef _DEBUG || !defined(_MSC_VER) 
+        wxHandleFatalExceptions();
+    #endif
 #else
     SetUnhandledExceptionFilter(windows_exception_handler);
 #endif
