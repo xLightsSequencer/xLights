4b4a2ee595efdf8ee8e18ac08e537ecb913714a5
keithsw1111 <keithsw1111@hotmail.com>
2019-12-26 20:01:29 +1100

    Upgrade Inno Setup to v6

--- build_scripts/msw/PackageWindowsRelease_VS-x64Only.cmd
+++ build_scripts/msw/PackageWindowsRelease_VS-x64Only.cmd
@@ -1,3 +1,3 @@
 call build_VS_x64.cmd 1
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" xLights_4_64bit_VS.iss
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" xLights_4_64bit_VS.iss
 pause
--- build_scripts/msw/PackageWindowsRelease_VS.cmd
+++ build_scripts/msw/PackageWindowsRelease_VS.cmd
@@ -1,7 +1,7 @@
 call build_VS_x64.cmd 1
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" xLights_4_64bit_VS.iss
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" xLights_4_64bit_VS.iss
 
 call build_VS_x86.cmd 1
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" xLights_4_32bit_VS.iss
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" xLights_4_32bit_VS.iss
 
 pause
