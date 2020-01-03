dad87e0ebbacee7ff9665e319220410a35d0fe90
keithsw1111 <keithsw1111@hotmail.com>
2020-01-03 13:55:49 +1100

    Start to get appveyor going again

--- appveyor.bat
+++ appveyor.bat
@@ -1,19 +1,18 @@
 echo on
 
+set FROMSDK=10.0.16299.0
+set TOSDK=10.0.14393.0
+
 dir "\program files (x86)\windows kits\10\bin\"
 
-rem dir c:\
-rem dir c:\mingw
-rem dir C:\mingw-w64
-
 dir
-
 set "xlightsdir=%cd%"
 
-rem git clone -q --branch=master https://github.com/wxWidgets/wxWidgets.git \projects\wxWidgets
-7z x -o\projects\wxWidgets wxWidgets-3.1.1.zip
+rem Install wxWidgets
+7z x -o\projects\wxWidgets wxWidgets-3.1.3.zip
 if %ERRORLEVEL% NEQ 0 exit 1
 
+rem Complete edits
 rem dir \projects\wxWidgets
 
 sed -i '#define wxUSE_STD_STRING_CONV_IN_WXSTRING wxUSE_STL/#define wxUSE_STD_STRING_CONV_IN_WXSTRING 1/g' \projects\wxWidgets\include\wx\msw\setup.h
@@ -22,6 +21,7 @@ if %ERRORLEVEL% NEQ 0 exit 1
 sed -i '#define wxUSE_STD_CONTAINERS 0/#define wxUSE_STD_CONTAINERS 1/g' \projects\wxWidgets\include\wx\msw\setup.h
 if %ERRORLEVEL% NEQ 0 exit 1
 
+rem Setup Inno Setup
 is.exe /LOG=inno.log /SUPPRESSMSGBOXES /SP- /NORESTART /NOCLOSEAPPLICATIONS /DIR=\projects\inno /NOICONS /SILENT
 if %ERRORLEVEL% NEQ 0 exit 1
 
@@ -29,46 +29,38 @@ type inno.log
 
 dir \projects\inno
 
-if [%BUILDTYPE%] EQU [X64DEBUGVS] goto x64DebugVS 
-if [%BUILDTYPE%] EQU [X64RELEASEGCC] goto x64ReleaseGCC 
-if [%BUILDTYPE%] EQU [X86RELEASEGCC] goto x86ReleaseGCC 
-
-echo "Unknown build type " %BUILDTYPE%
-
-exit 1
-
 rem =========================================== 64 BIT MSVC ===========================================
-:x64DebugVS
+:x64ReleaseVS
 
 rem prepare the header files for wxWidgets
 cd ..\wxWidgets\build\msw
 msbuild /m wx_custom_build.vcxproj /p:PlatformToolset=v%PLATFORMTOOLSET% /p:Configuration="Release"
 if %ERRORLEVEL% NEQ 0 exit 1
 
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_custom_build.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxpng.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxjpeg.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxzlib.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxtiff.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxexpat.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxscintilla.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_wxregex.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_base.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_net.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_core.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_xml.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_gl.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_html.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_media.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_adv.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_webview.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_qa.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_stc.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_xrc.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_richtext.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_aui.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_ribbon.vcxproj
-sed -i "s/10.0.16299.0/10.0.14393.0/g" wx_propgrid.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_custom_build.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxpng.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxjpeg.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxzlib.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxtiff.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxexpat.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxscintilla.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_wxregex.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_base.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_net.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_core.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_xml.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_gl.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_html.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_media.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_adv.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_webview.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_qa.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_stc.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_xrc.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_richtext.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_aui.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_ribbon.vcxproj
+sed -i "s/%FROMSDK%/%TOSDK%/g" wx_propgrid.vcxproj
 
 rem Build wxWidgets
 msbuild /m wx_vc15.sln /p:PlatformToolset=v%PLATFORMTOOLSET%
@@ -78,127 +70,31 @@ cd %xlightsdir%
 
 cd xLights
 
-sed -i "s/10.0.16299.0/10.0.14393.0/g" Xlights.vcxproj
-
-msbuild /m xLights.sln /p:PlatformToolset=v%PLATFORMTOOLSET%
-if %ERRORLEVEL% NEQ 0 exit 1
+sed -i "s/%FROMSDK%/%TOSDK%/g" Xlights.vcxproj
 
 cd ..
-
 cd xSchedule
-
-sed -i "s/10.0.16299.0/10.0.14393.0/g" xSchedule.vcxproj
-
-msbuild /m xSchedule.sln /p:PlatformToolset=v%PLATFORMTOOLSET%
+sed -i "s/%FROMSDK%/%TOSDK%/g" xSchedule.vcxproj
 if %ERRORLEVEL% NEQ 0 exit 1
 
-exit 0
-
-rem =========================================== 32 BIT GCC ===========================================
-:x86ReleaseGCC
-
-rem extract the 32 bit compiler
-7z x -o\mingw32 i686-7.2.0-release-posix-dwarf-rt_v5-rev0.7z
-if %ERRORLEVEL% NEQ 0 exit 1
-
-dir c:\mingw32
-
-rem extract codeblocks
-7z x -o\codeblocks codeblocks-17.12-nosetup.zip
-
-dir c:\codeblocks
-
-rem This has to be cmd.exe because the makefile uses \ in paths and bash and sh dont understand them
-set MINGWPATH=C:\mingw32\mingw32\bin
-set PATH=%MINGWPATH%;%PATH%
-
-rem set
-
-cd ..\wxWidgets\build\msw
-
-rem make the header files
-mingw32-make setup_h -f makefile.gcc --debug MONOLITHIC=1 SHARED=1 UNICODE=1 CXXFLAGS="-std=gnu++14" BUILD=release SHELL=%COMSPEC%
-
-cd %xlightsdir%
-
-\codeblocks\codeblocks.exe --build --no-check-associations --no-splash-screen --target=MinGW_Release xfade\xfade.cbp > xfade.log
-
-dir bin
-
-type xfade.log
-
-rem 7z e cbp2make-stl-rev147-all.tar.7z -o. cbp2make-stl-rev147-all\bin\Release\cbp2make.exe
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-cd xlights
-
-rem ..\cbp2make.exe -in xLights.cbp -out xLights.cbp.mak --with-deps --keep-outdir --keep-objdir -windows -targets MinGW_Release
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-rem sed -i "s/-l\.\./../g" xLights.cbp.mak
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-rem mingw32-make -f xLights.cbp.mak CXXFLAGS="-std=gnu++14" WX=\projects\wxWidgets INC=-I\projects\wxWidgets\include -j 10 mingw_release
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-rem cd ..\xSchedule
-
-rem ..\cbp2make.exe -in xSchedule.cbp -out xSchedule.cbp.mak --with-deps --keep-outdir --keep-objdir -windows -targets MinGW_Release
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-rem mingw32-make -f xSchedule.cbp.mak CXXFLAGS="-std=gnu++14" WX=\projects\wxWidgets INC=-I\projects\wxWidgets\include -j 10 mingw_release
-rem if %ERRORLEVEL% NEQ 0 exit 1
-
-cd ..
-
-rem build the install file
-\projects\inno\iscc.exe "xLights_4_32bit.iss"
-
-exit 0
-
-rem =========================================== 64 BIT GCC ===========================================
-:x64ReleaseGCC
-
-set COMSPEC=cmd.exe
-set MINGWPATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin
-set PATH=%MINGWPATH%;%PATH%
-
-cd ..\wxWidgets\build\msw
-
-rem make the header files
-mingw32-make setup_h -f makefile.gcc --debug TARGET_CPU=X64 MONOLITHIC=1 SHARED=1 UNICODE=1 CXXFLAGS="-std=gnu++14" BUILD=release SHELL=%COMSPEC%
-
-cd %xlightsdir%
-
-7z e cbp2make-stl-rev147-all.tar.7z -o. cbp2make-stl-rev147-all\bin\Release\cbp2make.exe
-if %ERRORLEVEL% NEQ 0 exit 1
-
-cd xlights
-
-sed -i "s/gnu_gcc_64bit_compiler/gcc/g" xLights.cbp
-
-..\cbp2make.exe -in xLights.cbp -out xLights.cbp.mak --with-deps --keep-outdir --keep-objdir -windows -targets "64bit  MinGW_Release"
-if %ERRORLEVEL% NEQ 0 exit 1
-
-sed -i "s/-l\.\./../g" xLights.cbp.mak
-if %ERRORLEVEL% NEQ 0 exit 1
-
-mingw32-make -f xLights.cbp.mak CXXFLAGS="-std=gnu++14" WX=\projects\wxWidgets INC=-I\projects\wxWidgets\include -j 10 64bit__mingw_release
-if %ERRORLEVEL% NEQ 0 exit 1
-
-cd ..\xSchedule
-
-..\cbp2make.exe -in xSchedule.cbp -out xSchedule.cbp.mak --with-deps --keep-outdir --keep-objdir -windows -targets "64bit  MinGW_Release"
-if %ERRORLEVEL% NEQ 0 exit 1
-
-mingw32-make -f xSchedule.cbp.mak CXXFLAGS="-std=gnu++14" WX=\projects\wxWidgets INC=-I\projects\wxWidgets\include -j 10 64bit__mingw_release
+cd xSMSDaemon
+sed -i "s/%FROMSDK%/%TOSDK%/g" xSMSDaemon.vcxproj
 if %ERRORLEVEL% NEQ 0 exit 1
 
 cd ..
+cd ..
+cd xFade
+sed -i "s/%FROMSDK%/%TOSDK%/g" xFade.vcxproj
+if %ERRORLEVEL% NEQ 0 exit 1
 
-rem build the install file
-\projects\inno\iscc.exe "xLights_4_64bit.iss"
+cd ..
+cd xCapture
+sed -i "s/%FROMSDK%/%TOSDK%/g" xFade.vcxproj
+if %ERRORLEVEL% NEQ 0 exit 1
 
-dir
+cd ..
+cd build_scripts
+cd msw
+call PackageWindowsRelease_VS-x64Only.cmd
 
 exit 0
