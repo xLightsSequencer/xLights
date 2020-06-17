504e870d02fd83f34b999eec31307a87785fb257
priemd <david@thepriems.com>
2020-06-16 20:06:06 -0400

    new menu option to add selections to existing grp

--- xLights/LayoutPanel.h
+++ xLights/LayoutPanel.h
@@ -303,6 +303,7 @@ class LayoutPanel: public wxPanel
         NewModelBitmapButton* AddModelButton(const std::string &type, const char *imageData[]);
         void UpdateModelsForPreview(const std::string &group, LayoutGroup* layout_grp, std::vector<Model *> &prev_models, bool filtering );
         void CreateModelGroupFromSelected();
+        void AddSelectedToExistingGroup();
         void BulkEditControllerName();
         void BulkEditActive(bool active);
         void BulkEditTagColour();
@@ -504,6 +505,7 @@ class LayoutPanel: public wxPanel
         static const long ID_MNU_MAKEALLSCVALID;
         static const long ID_MNU_MAKEALLSCNOTOVERLAPPING;
         static const long ID_MNU_ADD_MODEL_GROUP;
+        static const long ID_MNU_ADD_TO_EXISTING_GROUP;
         void OnModelsPopup(wxCommandEvent& event);
 		LayoutGroup* GetLayoutGroup(const std::string &name);
 		const wxString& GetBackgroundImageForSelectedPreview();
