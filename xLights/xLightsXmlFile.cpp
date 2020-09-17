42ccb40837b51505cffe229f417ddd19f9398dd8
keithsw1111 <keithsw1111@hotmail.com>
2020-09-18 07:43:04 +1000

    Fix crash.

--- xLights/xLightsXmlFile.cpp
+++ xLights/xLightsXmlFile.cpp
@@ -71,7 +71,6 @@ xLightsXmlFile::~xLightsXmlFile()
 	if (audio != nullptr)
 	{
         ValueCurve::SetAudio(nullptr);
-        ValueCurve::SetSequenceElements(nullptr);
 		delete audio;
 		audio = nullptr;
 	}
