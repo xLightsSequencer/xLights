b95785b5d7aeb73ab3a931bbc06d8a794b06c1e9
Daniel Kulp <dan@kulp.com>
2020-11-13 15:21:44 -0500

    Cap "Per Preview" render buffers to max of 2048 in either direction to avoid gigantic buffers that will take ages to render

--- xLights/models/Model.cpp
+++ xLights/models/Model.cpp
@@ -3441,8 +3441,7 @@ void Model::InitRenderBufferNodes(const std::string &type, const std::string &ca
         std::list<float> outy;
 
         // For 3D render view buffers recursively process each individual model...should be able to handle nested model groups
-        if (GetDisplayAs() == "ModelGroup" && camera != "2D")
-        {
+        if (GetDisplayAs() == "ModelGroup" && camera != "2D") {
             std::vector<Model *> models;
             wxArrayString mn = wxSplit(ModelXml->GetAttribute("models"), ',');
             int nc = 0;
@@ -3468,8 +3467,7 @@ void Model::InitRenderBufferNodes(const std::string &type, const std::string &ca
         }
 
         for (int x = firstNode; x < newNodes.size(); x++) {
-            if (newNodes[x] == nullptr)
-            {
+            if (newNodes[x] == nullptr) {
                 logger_base.crit("CCC Model::InitRenderBufferNodes newNodes[x] is null ... this is going to crash.");
                 wxASSERT(false);
             }
@@ -3477,25 +3475,18 @@ void Model::InitRenderBufferNodes(const std::string &type, const std::string &ca
                 float sx = it2.screenX;
                 float sy = it2.screenY;
 
-                if (ModelXml == nullptr)
-                {
+                if (ModelXml == nullptr) {
                     // if model xml is null then this isnt a real model which means it doesnt have a real location
                     // which means translatePoint is going to do strange things ... so dont call it
-                }
-                else
-                {
-                    if (pcamera == nullptr || camera == "2D")
-                    {
+                } else {
+                    if (pcamera == nullptr || camera == "2D") {
                         // Handle all of the 2D classic transformations
                         //float sz = 0;
                         // reintroducing the z coordinate as otherwise with some rotations we end up with a zero width buffer
                         float sz = it2.screenZ;
                         GetModelScreenLocation().TranslatePoint(sx, sy, sz);
-                    }
-                    else
-                    {
-                        if (GetDisplayAs() != "ModelGroup")  // ignore for groups since they will have already calculated their node positions from recursion call above
-                        {
+                    } else {
+                        if (GetDisplayAs() != "ModelGroup") { // ignore for groups since they will have already calculated their node positions from recursion call above
                             // Handle 3D from an arbitrary camera position
                             float sz = it2.screenZ;
                             GetModelScreenLocation().TranslatePoint(sx, sy, sz);
@@ -3534,20 +3525,24 @@ void Model::InitRenderBufferNodes(const std::string &type, const std::string &ca
         // exteme locations which translate into crazy sized render buffers
         // this allows us to scale it back to the desired grid size
         float factor = 1.0;
-        if (pcamera != nullptr && camera != "2D" && GetDisplayAs() == "ModelGroup" && type == PER_PREVIEW)
-        {
+        if (pcamera != nullptr && camera != "2D" && GetDisplayAs() == "ModelGroup" && type == PER_PREVIEW) {
             int maxDimension = ((ModelGroup*)this)->GetGridSize();
-            if (maxDimension != 0 && (maxX - minX > maxDimension || maxY - minY > maxDimension))
-            {
+            if (maxDimension != 0 && (maxX - minX > maxDimension || maxY - minY > maxDimension)) {
                 // we need to resize all the points by this amount
                 factor = std::max(((float)(maxX - minX)) / (float)maxDimension, ((float)(maxY - minY)) / (float)maxDimension);
                 // But if it is already smaller we dont want to make it bigger
-                if (factor < 1.0)
-                {
+                if (factor < 1.0) {
                     factor = 1.0;
                 }
             }
         }
+        if (((maxX - minX) > 2048) || ((maxY - minY) > 2048)){
+            // this will result in a GIANT render buffer, lets reduce to something we can reasonably render
+            float fx = ((float)(maxX - minX)) / 2048.0f;
+            float fy = ((float)(maxY - minY)) / 2048.0f;
+            factor = fx > fy ? fx : fy;
+        }
+        
 
         minX /= factor;
         maxX /= factor;
@@ -3570,8 +3565,7 @@ void Model::InitRenderBufferNodes(const std::string &type, const std::string &ca
             auto itx = outx.begin();
             auto ity = outy.begin();
             for (int x = firstNode; x < newNodes.size(); x++) {
-                if (newNodes[x] == nullptr)
-                {
+                if (newNodes[x] == nullptr) {
                     logger_base.crit("DDD Model::InitRenderBufferNodes newNodes[x] is null ... this is going to crash.");
                     wxASSERT(false);
                 }
