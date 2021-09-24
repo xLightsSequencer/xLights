182c8005add289e1538474e4caf3e3ff24bd9790
Scott H <computergeek1507@users.noreply.github.com>
2021-09-24 14:39:18 -0400

    Fixing build, Keith gave too many f's

--- xLights/controllers/Pixlite16.cpp
+++ xLights/controllers/Pixlite16.cpp
@@ -480,7 +480,7 @@ int Pixlite16::PrepareV4Config(uint8_t* data) const
         mn = "PixLite" + _config._modelName.substr(6);
     }
 
-    WriteString(data, pos, _config._modelNameLen, mn);f
+    WriteString(data, pos, _config._modelNameLen, mn);
     data[pos++] = _config._dhcp;
     WriteString(data, pos, _config._nicknameLen, _config._nickname);
     for (int i = 0; i < _config._numOutputs; i++) { Write16(data, pos, _config._outputPixels[i]); }
