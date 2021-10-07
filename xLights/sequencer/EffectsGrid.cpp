2f0d6b04d1001e6c33c84ef6200a849ab4be772a
keithsw1111 <keithsw1111@hotmail.com>
2021-10-07 21:24:51 +1100

    Fix #2836 at maximum zoom double clicking on effects rounds and results in imprecise selection.

--- xLights/sequencer/EffectsGrid.cpp
+++ xLights/sequencer/EffectsGrid.cpp
@@ -170,7 +170,7 @@ void EffectsGrid::mouseLeftDClick(wxMouseEvent& event)
     if (mSequenceElements == nullptr) {
         return;
     }
-    int selectedTimeMS = mTimeline->GetAbsoluteTimeMSfromPosition(event.GetX());
+    int selectedTimeMS = mTimeline->GetTimeMSfromPosition(event.GetX()); // GetAbsoluteTimeMSfromPosition(event.GetX());
 
     if (!event.ShiftDown()) {
         update_time = selectedTimeMS;
