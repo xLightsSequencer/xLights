bf8983885bd6c1fa6e7aec5e0468914e57fc0d4c
keithsw1111 <keithsw1111@hotmail.com>
2021-10-16 16:29:47 +1100

    Add ability to save and load DMX channel mappings

--- xLights/RemapDMXChannelsDialog.h
+++ xLights/RemapDMXChannelsDialog.h
@@ -11,6 +11,7 @@
  **************************************************************/
 
 //(*Headers(RemapDMXChannelsDialog)
+#include <wx/button.h>
 #include <wx/dialog.h>
 #include <wx/grid.h>
 #include <wx/scrolwin.h>
@@ -25,6 +26,10 @@ class RemapDMXChannelsDialog: public wxDialog
 		virtual ~RemapDMXChannelsDialog();
 
 		//(*Declarations(RemapDMXChannelsDialog)
+		wxButton* Button_Cancel;
+		wxButton* Button_LoadMapping;
+		wxButton* Button_Ok;
+		wxButton* Button_SaveMapping;
 		wxGrid* Grid1;
 		wxScrolledWindow* ScrolledWindow1;
 		//*)
@@ -34,12 +39,22 @@ class RemapDMXChannelsDialog: public wxDialog
 		//(*Identifiers(RemapDMXChannelsDialog)
 		static const long ID_GRID1;
 		static const long ID_SCROLLEDWINDOW1;
+		static const long ID_BUTTON_OK;
+		static const long ID_BUTTON_CANCEL;
+		static const long ID_BUTTON_SAVE_MAPPING;
+		static const long ID_BUTTON_LOAD_MAPPING;
 		//*)
 
 	private:
 
 		//(*Handlers(RemapDMXChannelsDialog)
+		void OnButton_OkClick(wxCommandEvent& event);
+		void OnButton_CancelClick(wxCommandEvent& event);
+		void OnButton_SaveMappingClick(wxCommandEvent& event);
+		void OnButton_LoadMappingClick(wxCommandEvent& event);
 		//*)
+
+		bool IsValidRemapping(const wxString& value);
 
 		DECLARE_EVENT_TABLE()
 };
