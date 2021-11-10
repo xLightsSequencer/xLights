f50860026633c5af112e9d6638f35bb787842db1
Daniel Kulp <dan@kulp.com>
2021-11-10 17:31:54 -0500

    Change include to relative path

--- xLights/graphics/xlGraphicsBase.h
+++ xLights/graphics/xlGraphicsBase.h
@@ -4,9 +4,9 @@
 
 #if !defined(FORCE_OPENGL_BASE) && __has_include("osxUtils/wxMetalCanvas.hpp")
     // Metal base classes are available, use the MetalCanvas
-    #include "graphics/metal/xlMetalCanvas.h"
+    #include "metal/xlMetalCanvas.h"
     typedef xlMetalCanvas GRAPHICS_BASE_CLASS;
 #else
-    #include "graphics/opengl/xlGLCanvas.h"
+    #include "opengl/xlGLCanvas.h"
     typedef xlGLCanvas GRAPHICS_BASE_CLASS;
 #endif
