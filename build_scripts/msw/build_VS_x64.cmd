04f5ebab916c72da8579a955f003f3757d886cd5
keithsw1111 <keithsw1111@hotmail.com>
2022-05-08 09:14:28 +1000

    Archive the PDB files when building a release.

--- build_scripts/msw/build_VS_x64.cmd
+++ build_scripts/msw/build_VS_x64.cmd
@@ -22,25 +22,40 @@ msbuild.exe -m:10 xlDo.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
+mkdir build_scripts\msw\xlDo
+copy xlDo\x64\Release\*.pdb build_scripts\msw\xlDo
+
 cd xFade
 msbuild.exe -m:10 xFade.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
+
+mkdir build_scripts\msw\xFade
+copy xFade\x64\Release\*.pdb build_scripts\msw\xFade
 
 cd xCapture
 msbuild.exe -m:10 xCapture.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
+mkdir build_scripts\msw\xCapture
+copy xCapture\x64\Release\*.pdb build_scripts\msw\xCapture
+
 cd xScanner
 msbuild.exe -m:10 xScanner.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
+mkdir build_scripts\msw\xScanner
+copy xScanner\x64\Release\*.pdb build_scripts\msw\xScanner
+
 cd xSchedule
 msbuild.exe -m:10 xSchedule.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
+
+mkdir build_scripts\msw\xSchedule
+copy xSchedule\x64\Release\*.pdb build_scripts\msw\xSchedule
 
 cd xSchedule
 cd xSMSDaemon
@@ -49,6 +64,9 @@ if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 cd ..
 
+mkdir build_scripts\msw\xSMSDaemon
+copy xSchedule\xSMSDaemon\x64\Release\*.pdb build_scripts\msw\xSMSDaemon
+
 cd xSchedule
 cd RemoteFalcon
 msbuild.exe -m:10 RemoteFalcon.sln -p:Configuration="Release" -p:Platform="x64"
@@ -56,10 +74,16 @@ if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 cd ..
 
+mkdir build_scripts\msw\RemoteFalcon
+copy xScedhule\RemoteFalcon\x64\Release\*.pdb build_scripts\msw\RemoteFalcon
+
 cd xLights
 msbuild.exe -m:10 xLights.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
+
+mkdir build_scripts\msw\xLights
+copy xLights\x64\Release\*.pdb build_scripts\msw\xLights
 
 cd build_scripts
 cd msw
