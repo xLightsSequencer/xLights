7018f4a2d87ca52cb01d3b3ed223f759617ee14f
Scott Hanson <scooter_seh@yahoo.com>
2022-10-31 10:13:13 -0400

    update Experience Lights max pixel count to be unrealistically high, updated not libltc build script

--- build_scripts/msw/build_VS_x64_skip_libltc.cmd
+++ build_scripts/msw/build_VS_x64_skip_libltc.cmd
@@ -1,3 +1,5 @@
+set cwd=%CD%
+
 IF NOT EXIST "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\amd64" GOTO Preview
 set PATH=C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\amd64;%PATH%
 Echo VS Professional Detected
@@ -10,6 +12,7 @@ Echo VS Preview Detected
 GOTO Start
 
 :Community
+IF NOT EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64" GOTO Start
 set PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64;%PATH%
 Echo VS Community Detected
 :Start
@@ -30,6 +33,9 @@ TipOfDay\Tool\x64\Release\PrepTOD.exe TipOfDay
 cd xlDo
 msbuild.exe -m:10 xlDo.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xlDo.map ..\bin64\xlDo.map
+if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
 mkdir build_scripts\msw\xlDo
@@ -37,6 +43,9 @@ copy xlDo\x64\Release\*.pdb build_scripts\msw\xlDo
 
 cd xFade
 msbuild.exe -m:10 xFade.sln -p:Configuration="Release" -p:Platform="x64"
+if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xFade.map ..\bin64\xFade.map
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
@@ -46,6 +55,9 @@ copy xFade\x64\Release\*.pdb build_scripts\msw\xFade
 cd xCapture
 msbuild.exe -m:10 xCapture.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xCapture.map ..\bin64\xCapture.map
+if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
 mkdir build_scripts\msw\xCapture
@@ -53,6 +65,9 @@ copy xCapture\x64\Release\*.pdb build_scripts\msw\xCapture
 
 cd xScanner
 msbuild.exe -m:10 xScanner.sln -p:Configuration="Release" -p:Platform="x64"
+if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xScanner.map ..\bin64\xScanner.map
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
@@ -62,6 +77,9 @@ copy xScanner\x64\Release\*.pdb build_scripts\msw\xScanner
 cd xSchedule
 msbuild.exe -m:10 xSchedule.sln -p:Configuration="Release" -p:Platform="x64" /p:DefineConstants="SKIP_SMPTE"
 if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xSchedule.map ..\bin64\xSchedule.map
+if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
 mkdir build_scripts\msw\xSchedule
@@ -70,6 +88,9 @@ copy xSchedule\x64\Release\*.pdb build_scripts\msw\xSchedule
 cd xSchedule
 cd xSMSDaemon
 msbuild.exe -m:10 xSMSDaemon.sln -p:Configuration="Release" -p:Platform="x64"
+if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xSMSDaemon.map ..\..\bin64\xSMSDaemon.map
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 cd ..
@@ -81,6 +102,9 @@ cd xSchedule
 cd RemoteFalcon
 msbuild.exe -m:10 RemoteFalcon.sln -p:Configuration="Release" -p:Platform="x64"
 if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\RemoteFalcon.map ..\..\bin64\RemoteFalcon.map
+if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 cd ..
 
@@ -89,6 +113,9 @@ copy xSchedule\RemoteFalcon\x64\Release\*.pdb build_scripts\msw\RemoteFalcon
 
 cd xLights
 msbuild.exe -m:10 xLights.sln -p:Configuration="Release" -p:Platform="x64"
+if %ERRORLEVEL% NEQ 0 goto error
+
+%cwd%\prepmap x64\Release\xLights.map ..\bin64\xLights.map
 if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
