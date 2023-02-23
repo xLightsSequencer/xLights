8964aa3f2ba25b111ad3a846c110206112a10d50
merryoncherry <cherry@merryoncherry.org>
2023-02-22 20:26:09 -0500

    Just the buggiest

--- xLights/RenderBuffer.cpp
+++ xLights/RenderBuffer.cpp
@@ -1004,6 +1004,7 @@ void RenderBuffer::SetNodePixel(int nodeNum, const xlColor &color, bool dmx_igno
 }
 
 void RenderBuffer::CopyNodeColorsToPixels(std::vector<bool> &done) {
+#if 0 // Sometimes quicker but sometimes quite glitchy!
     parallel_for(0, Nodes.size(), [&](int n) {
         xlColor c;
         Nodes[n]->GetColor(c);
@@ -1017,6 +1018,21 @@ void RenderBuffer::CopyNodeColorsToPixels(std::vector<bool> &done) {
         }
 
     }, 500);
+#else
+    int nn = Nodes.size();
+    for (int n = 0; n < nn; ++n) {
+        xlColor c;
+        Nodes[n]->GetColor(c);
+        for (auto& a : Nodes[n]->Coords) {
+            int x = a.bufX;
+            int y = a.bufY;
+            if (x >= 0 && x < BufferWi && y >= 0 && y < BufferHt && y * BufferWi + x < pixelVector.size()) {
+                pixels[y * BufferWi + x] = c;
+                done[y * BufferWi + x] = true;
+            }
+        }
+    }
+#endif
 }
 
 
