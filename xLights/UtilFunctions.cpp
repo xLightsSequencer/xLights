7aa0a75fd9e96e15a130ef52da20bb3fb0bd915c
merryoncherry <cherry@merryoncherry.org>
2023-02-24 23:19:52 -0500

    More fixes for elided nodes at start of strand

--- xLights/UtilFunctions.cpp
+++ xLights/UtilFunctions.cpp
@@ -1476,27 +1476,22 @@ wxString CompressNodes(const wxString& nodes)
     int last = -1;
     auto as = wxSplit(s, ',');
 
-    bool first = true; // There is no difference between empty row and row with one blank pixel (shrug)
+    // There is no difference between empty row and row with one blank pixel (shrug)
+    // We will take removal of the last comma approach
 
     for (const auto& i : as)
     {
         if (i.empty()) {
             // Flush out start/last if any
             if (start != -1) {
-                if (res != "") {
-                    res += ",";
-                }
                 if (last != start) {
-                    res += wxString::Format("%d-%d", start, last);                    
+                    res += wxString::Format("%d-%d,", start, last);                    
                 } else {
-                    res += wxString::Format("%d", start);                
+                    res += wxString::Format("%d,", start);                
                 }
             }
-            // Add separator and an empty
-            if (!first) {
-                res += ",";
-            }
-            first = false;
+            // Add empty and separator
+            res += ",";
             start = last = -1;
             dir = 0;
             continue;
@@ -1522,8 +1517,7 @@ wxString CompressNodes(const wxString& nodes)
                 }
                 else
                 {
-                    if (res != "") res += ",";
-                    res += wxString::Format("%d", start);
+                    res += wxString::Format("%d,", start);
                     start = j;
                     dir = 0;
                 }
@@ -1536,15 +1530,13 @@ wxString CompressNodes(const wxString& nodes)
                 }
                 else
                 {
-                    if (res != "") res += ",";
-                    res += wxString::Format("%d-%d", start, last);
+                    res += wxString::Format("%d-%d,", start, last);
                     start = j;
                     dir = 0;
                 }
                 last = j;
             }
         }
-        first = false;
     }
 
     if (start == -1)
@@ -1553,14 +1545,15 @@ wxString CompressNodes(const wxString& nodes)
     }
     else if (start == last)
     {
-        if (res != "") res += ",";
-        res += wxString::Format("%d", start);
+        res += wxString::Format("%d,", start);
     }
     else
     {
-        if (res != "") res += ",";
-        res += wxString::Format("%d-%d", start, last);
+        res += wxString::Format("%d-%d,", start, last);
     }
+
+    if (!res.empty())
+        res = res.substr(0, res.length() - 1); // Chop last comma
 
     return res;
 }
@@ -1571,6 +1564,7 @@ wxString ExpandNodes(const wxString& nodes)
 
     auto as = wxSplit(nodes, ',');
 
+    bool first = true;
     for (const auto& i : as)
     {
         if (i.Contains("-"))
@@ -1584,20 +1578,20 @@ wxString ExpandNodes(const wxString& nodes)
                 {
                     for (int j = start; j <= end; j++)
                     {
-                        if (res != "") res += ",";
+                        if (!first || res != "") res += ",";
                         res += wxString::Format("%d", j);
                     }
                 }
                 else if (start == end)
                 {
-                    if (res != "") res += ",";
+                    if (!first || res != "") res += ",";
                     res += wxString::Format("%d", start);
                 }
                 else
                 {
                     for (int j = start; j >= end; j--)
                     {
-                        if (res != "") res += ",";
+                        if (!first || res != "") res += ",";
                         res += wxString::Format("%d", j);
                     }
                 }
@@ -1605,9 +1599,10 @@ wxString ExpandNodes(const wxString& nodes)
         }
         else
         {
-            if (res != "") res += ",";
+            if (!first || res != "") res += ",";
             res += i;
         }
+        first = false;
     }
     return res;
 }
