82ad4cd7cd63d5a225656ea27077a143c66fa144
Daniel Kulp <dan@kulp.com>
2023-04-23 15:41:35 -0400

    FPP Connect - Upload media directly to appropriate directory

--- xLights/controllers/FPP.cpp
+++ xLights/controllers/FPP.cpp
@@ -71,6 +71,18 @@ using namespace TraceLog;
 
 static const std::string LEDPANELS("LED Panels");
 
+
+static std::set<std::string> FPP_MEDIA_EXT = {
+    "mp3", "ogg", "m4a", "m4p", "wav", "au", "wma", "flac", "aac",
+    "MP3", "OGG", "M4A", "M4P", "WAV", "AU", "WMA", "FLAC", "AAC",
+    "mp4", "MP4", "avi", "AVI", "mov", "MOV", "mkv", "MKV",
+    "mpg", "MPG", "mpeg", "MPEG"
+};
+static std::set<std::string> FPP_VIDEO_EXT = {
+    "mp4", "MP4", "avi", "AVI", "mov", "MOV", "mkv", "MKV",
+    "mpg", "MPG", "mpeg", "MPEG"
+};
+
 FPP::FPP(const std::string& ad) :
     BaseController(ad, ""), majorVersion(0), minorVersion(0), patchVersion(0), outputFile(nullptr), parent(nullptr), ipAddress(ad), curl(nullptr), fppType(FPP_TYPE::FPP) {
     wxIPV4address address;
@@ -891,19 +903,12 @@ int progress_callback(void *clientp,
     }
     return 0;
 }
+
 bool FPP::uploadFileV7(const std::string &filename,
                        const std::string &file,
-                       const std::string &d) {
+                       const std::string &dir) {
     bool cancelled = false;
-    bool callMove = false;
-    std::string dir = d;
-    if (dir == "music") {
-        // xLights treats all media as music so for now we'll upload to uploads and call move to have
-        // fpp sort out where it goes.
-        dir = "uploads";
-        callMove = true;
-    }
-    
+
     wxFile in;
     in.Open(file);
     if (in.IsOpened()) {
@@ -992,11 +997,6 @@ bool FPP::uploadFileV7(const std::string &filename,
             }
         }
     }
-    if (callMove) {
-        if (!callMoveFile(filename)) {
-            messages.push_back("ERROR Uploading file: " + filename + "     Could not move file to proper directory.");
-        }
-    }
     return cancelled;
 }
 
@@ -1080,13 +1080,6 @@ bool FPP::uploadOrCopyFile(const std::string &filename,
 // 5 - V2 zlib
 // 6 - V2 sparse zlib
 
-
-static std::set<std::string> FPP_MEDIA_EXT = {
-    "mp3", "ogg", "m4a", "m4p", "wav", "au", "wma", "flac", "aac",
-    "MP3", "OGG", "M4A", "M4P", "WAV", "AU", "WMA", "FLAC", "AAC",
-    "mp4", "MP4", "avi", "AVI", "mov", "MOV", "mkv", "MKV",
-    "mpg", "MPG", "mpeg", "MPEG"
-};
 
 static void FindHostSpecificMedia(const std::string &hostName, std::string &mediaBaseName, std::string &mediaFile, wxFileName &mfn) {
     wxFileName mfn2(mediaFile);
@@ -1181,7 +1174,13 @@ bool FPP::PrepareUploadSequence(const FSEQFile &file,
             }
         }
         if (doMediaUpload) {
-            cancelled |= uploadOrCopyFile(mediaBaseName, mediaFile, "music");
+            std::string dir = "music";
+            for (auto &a : FPP_VIDEO_EXT) {
+                if (mfn.GetExt() == a) {
+                    dir = "videos";
+                }
+            }
+            cancelled |= uploadOrCopyFile(mediaBaseName, mediaFile, dir);
         }
         if (cancelled) {
             return cancelled;
