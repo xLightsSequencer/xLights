a92f200641061e34c21ebd2f4b25a6b59fe4a0c1
Scott Hanson <scooter_seh@yahoo.com>
2023-07-10 16:48:10 -0400

    Added ability to set Model Properties through API

--- xLights/LayoutPanel.h
+++ xLights/LayoutPanel.h
@@ -479,6 +479,7 @@ class LayoutPanel: public wxPanel
         std::string GetSelectedModelName() const;
         bool Is3d() const;
         void Set3d(bool is3d);
+        wxPropertyGrid* GetPropertyEditor() const { return propertyEditor; }
 
     private:
         int Col_Model = 0;
