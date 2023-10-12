b47f89ad8bdc794441e985f4d1c1764a8b185011
Daniel Kulp <dan@kulp.com>
2023-10-12 09:40:55 -0400

    If the StartChannel attribute is different between show and base and it's autolayout by controller, don't consider it different as it will be recalculated anyway.

--- xLights/models/BaseObject.cpp
+++ xLights/models/BaseObject.cpp
@@ -250,31 +250,32 @@ void BaseObject::FlipVertical(bool ignoreLock) {
 
 bool BaseObject::IsXmlChanged(wxXmlNode* n) const
 {
-    for (wxXmlAttribute* a = n->GetAttributes(); a != nullptr; a = a->GetNext())
-    {
-        if (!ModelXml->HasAttribute(a->GetName()) || ModelXml->GetAttribute(a->GetName()) != a->GetValue())
-            return true;
+    for (wxXmlAttribute* a = n->GetAttributes(); a != nullptr; a = a->GetNext()) {
+        if (!ModelXml->HasAttribute(a->GetName()) || ModelXml->GetAttribute(a->GetName()) != a->GetValue()) {
+            if (a->GetName() != "StartChannel" || !ModelXml->HasAttribute("Controller")) {
+                return true;
+            }
+        }
     }
 
     // This part assumes the nodes under the model only exist once or if they exist twice they have a "Name" attribute that distinguishes them
     // it also assumes one level of child nodes only
-    for (wxXmlNode* nn = n->GetChildren(); nn != nullptr; nn = nn->GetNext())
-    {
+    for (wxXmlNode* nn = n->GetChildren(); nn != nullptr; nn = nn->GetNext()) {
         bool found = false;
-        for (wxXmlNode* cc = ModelXml->GetChildren(); cc != nullptr; cc = cc->GetNext())
-        {
-            if (cc->GetName() == nn->GetName() && (!nn->HasAttribute("name") || (cc->GetAttribute("name") == nn->GetAttribute("name"))))
-            {
+        for (wxXmlNode* cc = ModelXml->GetChildren(); cc != nullptr; cc = cc->GetNext()) {
+            if (cc->GetName() == nn->GetName() && (!nn->HasAttribute("name") || (cc->GetAttribute("name") == nn->GetAttribute("name")))) {
                 found = true;
                 for (wxXmlAttribute* a = cc->GetAttributes(); a != nullptr; a = a->GetNext()) {
-                    if (!cc->HasAttribute(a->GetName()) || nn->GetAttribute(a->GetName()) != a->GetValue())
+                    if (!cc->HasAttribute(a->GetName()) || nn->GetAttribute(a->GetName()) != a->GetValue()) {
                         return true;
+                    }
                 }
             }
         }
 
-        if (!found)
+        if (!found) {
             return true;
+        }
     }
 
     return false;
