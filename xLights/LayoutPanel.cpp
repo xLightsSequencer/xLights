6ec305d0a4f5b438985fff12b66b185d4911abbe
Kyle Grymonprez <1175063+kylegrymonprez@users.noreply.github.com>
2023-10-14 14:39:26 -0700

    Node Count Tooltip addition (#4051)
    * Adding tooltip addition for quick showing the amount of nodes in the tooltip for selected models on the layout tab
    
    * Changes to add node count to the bottom right of the window frame. Causes the filename text to shuffle around a little bit
    
    * Revert "Changes to add node count to the bottom right of the window frame. Causes the filename text to shuffle around a little bit"
    
    This reverts commit 7418e8e8b9cb2206264a492a4fb4646661183d65.
    
    * removed logging and reworked logic statements to reduce if/else statements
    
    ---------
    
    Co-authored-by: Kyle Grymonprez <>

--- xLights/LayoutPanel.cpp
+++ xLights/LayoutPanel.cpp
@@ -8499,11 +8499,13 @@ void LayoutPanel::HandleSelectionChanged() {
 
         if (totalSelections > 1) {
             showBackgroundProperties();
-            tooltip = wxString::Format("Selected Items:\n -Groups: %d\n -Models: %d\n -SubModels: %d\n", gSize, mSize, smSize);
+            tooltip = wxString::Format("Selected Items:\n -Groups: %d\n -Models: %d\n -SubModels: %d\n\nTotal Nodes: %d", gSize, mSize, smSize, calculateNodeCountOfSelected());
         } else if (gSize == 1) {
             Model* model = GetModelFromTreeItem(selectedTreeGroups[0]);
             if (model->IsFromBase()) {
                 tooltip = "From Base Show Folder";
+            } else {
+                tooltip = wxString::Format("Total Nodes in Group: %d", calculateNodeCountOfSelected());
             }
             ShowPropGrid(false);
             model_grp_panel->UpdatePanel(TreeListViewModels->GetItemText(selectedTreeGroups[0]));
@@ -8902,4 +8904,59 @@ bool LayoutPanel::IsNewModel(Model* m) const
     if (m == nullptr) return _newModel != nullptr;
 
     return m == _newModel;
+}
+
+//Calculate the total node count of selected items in the panel. Handles calculations of models, submodels and groups
+int LayoutPanel::calculateNodeCountOfSelected()
+{
+    int totalNodeCount = 0;
+    std::vector<Model*> modelsProcessed;
+    //We can break the selected groups into their components for processing. GetSelectedModelsForEdit already does this, even though we aren't editing. We can reuse that logic. This gives us all models, so I want to split this back up into models and submodels
+    std::vector<Model*> modelsToProcess = GetSelectedModelsForEdit();
+    std::vector<Model*> selectedModels;
+    std::vector<Model*> selectedSubModels;
+    
+    for (const auto& item : selectedTreeSubModels){
+        ModelTreeData *submodelData = (ModelTreeData*)TreeListViewModels->GetItemData(item);
+        Model* subModel = ((submodelData != nullptr) ? submodelData->GetModel() : nullptr);
+        if( subModel )
+            selectedSubModels.push_back(subModel);
+    }
+    
+    //Now parse the group elements into their perspective groups for processing
+    std::vector<Model*> sgModels = GetSelectedModelsForEdit();
+    for (const auto& item : sgModels){
+        if (item->GetDisplayAs() == "SubModel") {
+            selectedSubModels.push_back(item);
+        } else {
+            selectedModels.push_back(item);
+        }
+    }
+    
+    //Process the core models first. Save their pointer addresses for use in submodel and group processing
+    for (const auto& model : selectedModels) {
+        //If any of this models submodels are already counted, we shouldn't count the nodes from that submodel twice
+        if (model != nullptr) {
+            if(std::find(modelsProcessed.begin(), modelsProcessed.end(), model) == modelsProcessed.end()){
+                totalNodeCount += model->GetNodeCount();
+                modelsProcessed.push_back(model);
+            }
+        }
+    }
+    // Now process submodels. Submodels might already be counted from the above parent models, so dont process a submodel if it's parent is already processed and accounted for.
+    for (const auto& subModel : selectedSubModels) {
+        Model* parent_info = dynamic_cast<SubModel*>(subModel)->GetParent();
+        if (subModel != nullptr) {
+            //if this submodel is already in the count, dont do it
+            if(std::find(modelsProcessed.begin(), modelsProcessed.end(), parent_info) == modelsProcessed.end()
+               && std::find(modelsProcessed.begin(), modelsProcessed.end(), subModel) == modelsProcessed.end()
+               )
+            {
+                totalNodeCount += subModel->GetNodeCount();
+                modelsProcessed.push_back(subModel);
+            }
+        }
+    }
+    
+    return totalNodeCount;
 }
