bc1077228b3fa1eea25aff3e00b9c4a48d836cb5
Daniel Hacquebord <me@danielhac.com>
2023-12-01 06:54:21 -0500

    Fix height calculations for Arches and Candy Canes

--- xLights/models/ThreePointScreenLocation.cpp
+++ xLights/models/ThreePointScreenLocation.cpp
@@ -90,7 +90,7 @@ void ThreePointScreenLocation::AddDimensionProperties(wxPropertyGridInterface* p
     TwoPointScreenLocation::AddDimensionProperties(propertyEditor, 1.0);
     float width = RulerObject::Measure(origin, point2);
     wxPGProperty* prop = propertyEditor->Append(new wxFloatProperty(wxString::Format("Height (%s)", RulerObject::GetUnitDescription()), "RealHeight", 
-                                                                     RulerObject::Measure((width * height) / 2.0 * factor * 100.0)
+                                                                     (width * height) / 2.0 * factor
                                                                     ));
     prop->ChangeFlag(wxPG_PROP_READONLY, true);
     prop->SetAttribute("Precision", 2);
@@ -113,7 +113,7 @@ float ThreePointScreenLocation::GetRealWidth() const
 float ThreePointScreenLocation::GetRealHeight() const
 {
     float width = RulerObject::Measure(origin, point2);
-    return RulerObject::Measure((width * height) / 2.0 * 1.0 * 100.0);
+    return (width * height) / 2.0 * 1.0;
 }
 
 void ThreePointScreenLocation::AddSizeLocationProperties(wxPropertyGridInterface *propertyEditor) const {
