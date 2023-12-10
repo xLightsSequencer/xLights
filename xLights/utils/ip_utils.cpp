15000678fdb5bce15c2b26c3435141ae34255d12
Daniel Kulp <dan@kulp.com>
2023-12-10 13:16:06 -0500

    Fix potential crash/memory corruption at startup when resolving IP's on background threads

--- xLights/utils/ip_utils.cpp
+++ xLights/utils/ip_utils.cpp
@@ -17,10 +17,12 @@
 #include <wx/sckaddr.h>
 
 #include <map>
+#include <mutex>
 
 namespace ip_utils
 {
     static std::map<std::string, std::string> __resolvedIPMap;
+    static std::mutex __resolvedIPMapLock;
 
 	bool IsIPValid(const std::string& ip)
     {
@@ -103,6 +105,7 @@ namespace ip_utils
         if (IsIPValid(ip) || (ip == "MULTICAST") || ip == "" || StartsWith(ip, ".") || (ip[0] >= '0' && ip[0] <= '9')) {
             return ip;
         }
+        std::unique_lock<std::mutex> lock(__resolvedIPMapLock);
         const std::string& resolvedIp = __resolvedIPMap[ip];
         if (resolvedIp == "") {
             wxIPV4address add;
