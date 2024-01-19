e300902a923b00ae79dbf4b7c4e94c9ff48a856b
Scott Hanson <scooter_seh@yahoo.com>
2024-01-19 17:36:34 -0500

    fix

--- xLights/automation/PythonRunner.cpp
+++ xLights/automation/PythonRunner.cpp
@@ -37,15 +37,15 @@ PythonRunner::PythonRunner(xLightsFrame* frame) :
 
 std::string PythonRunner::GetUserScriptFolder() const
 {
-    return _frame->GetShowDirectory() + wxFileName::GetPathSeparator() + "scripts" + wxFileName::GetPathSeparator();
+    return _frame->GetShowDirectory() + GetPathSeparator() + "scripts" + GetPathSeparator();
 }
 
 std::string PythonRunner::GetSystemScriptFolder()
 {
 #ifndef __WXMSW__
-    return wxStandardPaths::Get().GetResourcesDir() + wxFileName::GetPathSeparator() + "scripts";
+    return wxStandardPaths::Get().GetResourcesDir() + GetPathSeparator() + "scripts";
 #else
-    return wxFileName(wxStandardPaths::Get().GetExecutablePath()).GetPath() + wxFileName::GetPathSeparator() + "scripts";
+    return wxFileName(wxStandardPaths::Get().GetExecutablePath()).GetPath() + GetPathSeparator() + "scripts";
 #endif
 }
 
@@ -90,7 +90,7 @@ std::list<std::string> PythonRunner::PromptSequences() const
         wxArrayString files = dlg.GetFileList();
 
         for (auto const& f : files) {
-            wxFileName fname(_frame->GetShowDirectory() + wxFileName::GetPathSeparator() + f);
+            wxFileName fname(_frame->GetShowDirectory() + GetPathSeparator() + f);
             if (FileExists(fname)) {
                 sequenceList.push_back(fname.GetFullPath());
             } else {
