9e83f2375d85acb9bb1c8c74a8cf481e6693eca0
Daniel Kulp <dan@kulp.com>
2024-01-19 17:23:22 -0500

    Stick the branch name into the whattotest file

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,4 +4,5 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
 	git log -1 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+    git status | head -n 1 >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
