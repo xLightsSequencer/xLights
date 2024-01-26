9c9663e29f8a56c7f6c6834c2c5322798a2306d4
Daniel Kulp <dan@kulp.com>
2024-01-26 10:06:53 -0500

    Try to echo what is put in the test flight file

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -3,6 +3,6 @@
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
-	git log --since="28 hours ago" --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-    git status | head -n 1 >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+    git log --since="28 hours ago" --pretty=format:"%s" | tee $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+    # git status | head -n 1 >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
