0a1b9113d6422ee5137be47266b2d6801ded5595
darylE <derwin12@users.noreply.github.com>
2024-02-09 16:19:17 -0500

    Dont initially expand the vendors on the load of the download models #4332

--- xLights/VendorModelDialog.cpp
+++ xLights/VendorModelDialog.cpp
@@ -938,7 +938,6 @@ bool VendorModelDialog::LoadTree(wxProgressDialog* prog, int low, int high)
         {
             AddHierachy(v, it, it->_categories);
         }
-        TreeCtrl_Navigator->Expand(v);
     }
 
     if (first.IsOk() && first != root)
