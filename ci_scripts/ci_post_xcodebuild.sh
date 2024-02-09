e0eaae2231dca449fbaeed5e05e4820f9c5c4d06
Daniel Kulp <dan@kulp.com>
2024-02-09 08:08:31 -0500

    Still not sure why only one log entry is appearing.  Attempt all from last tagy

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,5 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
     git status | head -n 1 | tee $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-    git log --since="48 hours ago" --pretty=format:"%s" | tee -a $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+    git log $(git describe --tags --abbrev=0 HEAD^)..HEAD --oneline | colrm 1 10 | tee -a $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
