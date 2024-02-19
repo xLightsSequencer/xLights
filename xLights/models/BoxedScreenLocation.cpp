ebb76f606b2332879e8acc1065e2034fc409c21b
darylE <derwin12@users.noreply.github.com>
2024-02-19 15:34:58 -0500

    Allow dimensions for the Image type #3817

--- xLights/models/BoxedScreenLocation.cpp
+++ xLights/models/BoxedScreenLocation.cpp
@@ -1286,10 +1286,18 @@ float BoxedScreenLocation::GetBack() const {
 }
 
 float BoxedScreenLocation::GetRestorableMWidth() const {
-    return (RenderWi-1) * scalex;
+    if (RenderWi == 1) {
+        return scalex;
+    } else {
+        return (RenderWi - 1) * scalex;
+    }
 }
 float BoxedScreenLocation::GetRestorableMHeight() const {
-    return (RenderHt-1) * scaley;
+    if (RenderHt == 1) {
+        return scaley;
+    } else {
+        return (RenderHt - 1) * scaley;
+    }
 }
 float BoxedScreenLocation::GetMWidth() const {
     return RenderWi * scalex;
@@ -1300,7 +1308,11 @@ float BoxedScreenLocation::GetMHeight() const {
 void BoxedScreenLocation::SetMWidth(float w) {
     if (RenderWi == 1)
     { 
-        scalex = 1;
+        if (w != 0) {
+            scalex = w;
+        } else {
+            scalex = 1;
+        }
     }
     else
     {
@@ -1326,7 +1338,11 @@ float BoxedScreenLocation::GetRestorableMDepth() const {
 void BoxedScreenLocation::SetMHeight(float h) {
     if (RenderHt == 1 || h == 0)
     {
-        scaley = 1;
+        if (h != 0) {
+            scaley = h;
+        } else {
+            scaley = 1;
+        }
     }
     else
     {
