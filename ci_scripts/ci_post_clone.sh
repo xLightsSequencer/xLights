ed9a03b46033f85f1b124e3e931f16a85cd28e94
Daniel Kulp <dan@kulp.com>
2024-02-27 11:01:29 -0500

    XcodeCloud - don't re-download dependencies every build, store in derived data

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,6 +2,14 @@
 env
 pwd
 
+cd $CI_DERIVED_DATA_PATH
+if [ ! -d xLights-macOS-dependencies.git ]; then
+    git clone https://github.com/xLightsSequencer/xLights-macOS-dependencies.git
+fi
+cd xLights-macOS-dependencies.git
+git reset --hard
+git pull
+
 cd ${CI_PRIMARY_REPOSITORY_PATH}/macOS
 rm -rf dependencies
-git clone https://github.com/xLightsSequencer/xLights-macOS-dependencies.git dependencies
+ln -s ${$CI_DERIVED_DATA_PATH}/xLights-macOS-dependencies.git dependencies
