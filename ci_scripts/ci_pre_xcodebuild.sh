d01b08ee5bfa03932cf2577b03a344ca93c9c6fe
Daniel Kulp <dan@kulp.com>
2024-02-27 12:11:01 -0500

    XcodeCloud - Move depenency download to pre_xcodebuild phase

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -4,13 +4,22 @@ pwd
 
 mkdir -p $CI_DERIVED_DATA_PATH
 cd $CI_DERIVED_DATA_PATH
+ls -lart
 if [ ! -d xLights-macOS-dependencies ]; then
     git clone https://github.com/xLightsSequencer/xLights-macOS-dependencies.git xLights-macOS-dependencies
 fi
 cd xLights-macOS-dependencies
 git reset --hard
 git pull
+cd ..
+echo ""
+echo "Post update"
+ls -lart
+
 
 cd ${CI_PRIMARY_REPOSITORY_PATH}/macOS
 rm -rf dependencies
-ln -s ${CI_DERIVED_DATA_PATH}/xLights-macOS-dependencies.git dependencies
+ln -s ${CI_DERIVED_DATA_PATH}/xLights-macOS-dependencies dependencies
+echo ""
+echo "Post link"
+ls -lart
