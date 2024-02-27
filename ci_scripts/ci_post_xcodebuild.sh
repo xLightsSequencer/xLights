72475b94b01be3c3097a03f649360cc4bdf57527
Daniel Kulp <dan@kulp.com>
2024-02-27 10:24:14 -0500

    Another attempt to get the list of changes

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
+git fetch --tags -f
+git status | head -n 1 | tee WhatToTest.en-US.txt
+git log $(git describe --abbrev=0 HEAD^)..HEAD --oneline | colrm 1 10 | tee -a WhatToTest.en-US.txt
+
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir $TESTFLIGHT_DIR_PATH
-    git status | head -n 1 | tee $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-    git log $(git describe --tags --abbrev=0 HEAD^)..HEAD --oneline | colrm 1 10 | tee -a $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+    cp -f WhatToTest.en-US.txt $TESTFLIGHT_DIR_PATH
 fi
