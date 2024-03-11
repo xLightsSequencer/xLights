52527188a02508712883c643e8f91333fd1115d4
Daryl <4643499+derwin12@users.noreply.github.com>
2024-03-11 15:24:03 -0400

    Add Text Effect option to color each word #4032 (#4394)
    * Add TEXT option to color each word #4032
    
    Co-authored-by: darylE <derwin12@users.noreply.github.com>

--- xLights/effects/TextEffect.cpp
+++ xLights/effects/TextEffect.cpp
@@ -322,6 +322,7 @@ void TextEffect::SetDefaultParameters() {
     SetChoiceValue(tp->Choice_Text_Count, "none");
     SetCheckboxValue(tp, tp->ID_CHECKBOX_TextToCenter, false);
     SetCheckBoxValue(tp->CheckBox_Text_PixelOffsets, false);
+    SetCheckboxValue(tp, tp->ID_CHECKBOX_Text_Color_PerWord, false);
     SetSliderValue(tp->Slider_Text_XStart, 0);
     SetSliderValue(tp->Slider_Text_YStart, 0);
     SetSliderValue(tp->Slider_Text_XEnd, 0);
@@ -537,6 +538,7 @@ void TextEffect::Render(Effect *effect, const SettingsMap &SettingsMap, RenderBu
         int endy = wxAtoi(SettingsMap.Get("SLIDER_Text_YEnd", "0"));
         int endx = wxAtoi(SettingsMap.Get("SLIDER_Text_XEnd", "0"));
         bool pixelOffsets = wxAtoi(SettingsMap.Get("CHECKBOX_Text_PixelOffsets", "0"));
+        bool perWord = wxAtoi(SettingsMap.Get("CHECKBOX_Text_Color_PerWord", "0"));
 
         wxImage * i = RenderTextLine(buffer,
                        buffer.GetTextDrawingContext(),
@@ -547,7 +549,7 @@ void TextEffect::Render(Effect *effect, const SettingsMap &SettingsMap, RenderBu
                        TextEffectsIndex(SettingsMap["CHOICE_Text_Effect"]),
                        TextCountDownIndex(SettingsMap["CHOICE_Text_Count"]),
                        wxAtoi(SettingsMap.Get("TEXTCTRL_Text_Speed", "10")),
-                       startx, starty, endx, endy, pixelOffsets);
+                       startx, starty, endx, endy, pixelOffsets, perWord);
         
         if (i == nullptr) {
             return;
@@ -736,7 +738,8 @@ void DrawLabel(TextDrawingContext *dc,
                int alignment,
                TextRenderCache *cache,
                const std::string &fontString,
-               const std::vector<xlColor> colors)
+               const std::vector<xlColor> colors,
+               const bool perWord = false)
 {
     // find the text position
     wxCoord widthText, heightText, heightLine;
@@ -811,7 +814,6 @@ void DrawLabel(TextDrawingContext *dc,
                         wxString c = curLine[x1];
                         if (c != " ") {
                             SetFont(dc, fontString, colors[curPos % colors.size()]);
-                            curPos++;
                             double loc = xRealStart;
                             if (x1 != 0) {
                                 if (x1 - 1 < d.size()) {
@@ -822,6 +824,9 @@ void DrawLabel(TextDrawingContext *dc,
                                 }
                             }
                             dc->DrawText(c, loc, y);
+                        }
+                        if ((perWord && c == ' ') || (!perWord && c != ' ')) {
+                            curPos++;
                         }
                     }
                 } else {
@@ -909,7 +914,7 @@ wxImage *TextEffect::RenderTextLine(RenderBuffer &buffer,
                                     int dir,
                                     bool center, int Effect, int Countdown, int tspeed,
                                     int startx, int starty, int endx, int endy,
-                                    bool isPixelBased) const
+                                    bool isPixelBased, bool perWord) const
 {
     int i;
     wxString Line = Line_orig;
@@ -1085,7 +1090,7 @@ wxImage *TextEffect::RenderTextLine(RenderBuffer &buffer,
         if (img == nullptr) {
             dc->Clear();
             SetFont(dc, fontString, colors[0]);
-            DrawLabel(dc, msg, rect, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, GetCache(buffer,id), fontString, colors);
+            DrawLabel(dc, msg, rect, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, GetCache(buffer,id), fontString, colors, perWord);
             wxImage *i2 = dc->FlushAndGetImage();
             img = new wxImage(i2->GetSize());
             *img = i2->Copy();
@@ -1459,6 +1464,7 @@ void TextEffect::RenderXLText(Effect* effect, const SettingsMap& settings, Rende
     int endy = wxAtoi(settings.Get("SLIDER_Text_YEnd", "0"));
     int endx = wxAtoi(settings.Get("SLIDER_Text_XEnd", "0"));
     bool pixelOffsets = wxAtoi(settings.Get("CHECKBOX_Text_PixelOffsets", "0"));
+    bool perWord = wxAtoi(settings.Get("CHECKBOX_Text_Color_PerWord", "0"));
 
     int OffsetLeft = startx * buffer.BufferWi / 100;
     int OffsetTop = -starty * buffer.BufferHt / 100;
@@ -1598,17 +1604,16 @@ void TextEffect::RenderXLText(Effect* effect, const SettingsMap& settings, Rende
     auto startOffsetLeft = OffsetLeft;
 
     if (text != "") {
-        int space_offset = 0;
+        int curPos = 0;
         for (int i = 0; i < text.length(); i++) {
-
             if (text[i] == '\n')
             {
                 OffsetLeft = startOffsetLeft;
                 OffsetTop += font->GetHeight() + 1;
             } else {
-                buffer.palette.GetColor((i - space_offset) % num_colors, c);
-                if (text[i] == ' ') {
-                    space_offset++;
+                buffer.palette.GetColor(curPos % num_colors, c);
+                if ((perWord && text[i] == ' ') || (!perWord && text[i] != ' ')) {
+                    curPos++;
                 }
                 char ascii = text[i];
                 int x_start_corner = (ascii % 8) * (char_width + 1) + 1;
