d7ca9275473e9a58c68e8660c47d315a70cf1ac6
darylE <derwin12@users.noreply.github.com>
2024-03-26 20:39:01 -0400

    Save and use 3d mode stored with layout #2138

--- xLights/LayoutPanel.cpp
+++ xLights/LayoutPanel.cpp
@@ -480,7 +480,7 @@ LayoutPanel::LayoutPanel(wxWindow* parent, xLightsFrame *xl, wxPanel* sequencer)
     is_3d = config->ReadBool("LayoutMode3D", false);
     bool allow_3d_previews = true; //false; //set to false for previous behavior
 
-    CheckBox_3D->SetValue(is_3d);
+    CheckBox_3D->SetValue(is_3d); 
     xlights->GetHousePreview()->Set3D(is_3d);
 
     if (!allow_3d_previews && is_3d)
@@ -8922,6 +8922,7 @@ void LayoutPanel::OnCheckBox_3DClick(wxCommandEvent& event)
 
     wxConfigBase* config = wxConfigBase::Get();
     config->Write("LayoutMode3D", is_3d);
+    xlights->SetXmlSetting("LayoutMode3D", is_3d ? "1" : "0");
     Refresh();
 }
 
