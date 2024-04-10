7a872b9c237ea1cd406cc47b50af676981ea750f
Daniel Kulp <dan@kulp.com>
2024-04-10 09:23:00 -0400

    Attempt to use a "release tgz"  instead of LFS data

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -17,13 +17,20 @@ ls -lart ${TMPDIR}
 mkdir -p $CI_DERIVED_DATA_PATH
 cd $CI_DERIVED_DATA_PATH
 ls -lart
-if [ ! -d xLights-macOS-dependencies ]; then
-    git clone --depth 1 https://github.com/xLightsSequencer/xLights-macOS-dependencies.git xLights-macOS-dependencies
-fi
-cd xLights-macOS-dependencies
-git reset --hard
-git pull
-cd ..
+
+# This will do a full clone which is huge and consumes a lot of bandwidth from GitHub LFS costing money
+#if [ ! -d xLights-macOS-dependencies ]; then
+#    git clone --depth 1 https://github.com/xLightsSequencer/xLights-macOS-dependencies.git xLights-macOS-dependencies
+#fi
+#cd xLights-macOS-dependencies
+#git reset --hard
+#git pull
+#cd ..
+
+#instead, grab the release tgz
+rm -rf xLights-macOS-dependencies
+export TAG=$(cat ${CI_PRIMARY_REPOSITORY_PATH}/macOS/README.macOS | grep recurse-submodules | cut -d "_" -f 2 | cut -d " "  -f 1)
+curl -L https://github.com/xLightsSequencer/xLights-macOS-dependencies/releases/download/xlights_${TAG}/xLights-macOS-dependencies-${TAG}.tgz --output - | tar -xz
 echo ""
 echo "Post update"
 ls -lart
