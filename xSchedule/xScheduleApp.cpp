625c6be4548c7d4b307357ee8d0cbbcb6bd850da
Scott Hanson <scooter_seh@yahoo.com>
2024-04-24 18:28:03 -0400

    fix xschedule post build

--- xSchedule/xScheduleApp.cpp
+++ xSchedule/xScheduleApp.cpp
@@ -103,9 +103,10 @@
 #pragma comment(lib, "avfilter.lib")
 #pragma comment(lib, "avformat.lib")
 #pragma comment(lib, "avutil.lib")
+#pragma comment(lib, "postproc.lib")
 #pragma comment(lib, "swresample.lib")
-#pragma comment(lib, "SDL2.lib")
 #pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "SDL2.lib")
 #endif
 
 IMPLEMENT_APP(xScheduleApp)
