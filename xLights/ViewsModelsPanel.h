188e89780255c30b9bca6e22c374352c3bf76430
Daryl <4643499+derwin12@users.noreply.github.com>
2024-10-28 10:43:15 -0400

    Tweak button to clarify and add tooltip #4868 (#4922)

--- xLights/ViewsModelsPanel.h
+++ xLights/ViewsModelsPanel.h
@@ -169,28 +169,28 @@ public:
 protected:
 
     //(*Identifiers(ViewsModelsPanel)
-    static const long ID_BUTTON3;
-    static const long ID_BUTTON4;
-    static const long ID_BUTTON5;
-    static const long ID_BUTTON6;
-    static const long ID_BUTTON_TOP;
-    static const long ID_BUTTON9;
-    static const long ID_BUTTON10;
-    static const long ID_BUTTON_BOTTOM;
-    static const long ID_BUTTON1;
-    static const long ID_BUTTON2;
-    static const long ID_BUTTON7;
-    static const long ID_BUTTON8;
-    static const long ID_BUTTON_IMPORT;
-    static const long ID_BUTTON11;
-    static const long ID_BUTTON_VIEWUP;
-    static const long ID_BUTTON_VIEWDOWN;
-    static const long ID_STATICTEXT1;
-    static const long ID_LISTCTRL_VIEWS;
-    static const long ID_STATICTEXT2;
-    static const long ID_LISTCTRL_MODELS;
-    static const long ID_LISTCTRL1;
-    static const long ID_STATICTEXT3;
+    static const wxWindowID ID_BUTTON3;
+    static const wxWindowID ID_BUTTON4;
+    static const wxWindowID ID_BUTTON5;
+    static const wxWindowID ID_BUTTON6;
+    static const wxWindowID ID_BUTTON_TOP;
+    static const wxWindowID ID_BUTTON9;
+    static const wxWindowID ID_BUTTON10;
+    static const wxWindowID ID_BUTTON_BOTTOM;
+    static const wxWindowID ID_BUTTON1;
+    static const wxWindowID ID_BUTTON2;
+    static const wxWindowID ID_BUTTON7;
+    static const wxWindowID ID_BUTTON8;
+    static const wxWindowID ID_BUTTON_IMPORT;
+    static const wxWindowID ID_BUTTON11;
+    static const wxWindowID ID_BUTTON_VIEWUP;
+    static const wxWindowID ID_BUTTON_VIEWDOWN;
+    static const wxWindowID ID_STATICTEXT1;
+    static const wxWindowID ID_LISTCTRL_VIEWS;
+    static const wxWindowID ID_STATICTEXT2;
+    static const wxWindowID ID_LISTCTRL_MODELS;
+    static const wxWindowID ID_LISTCTRL1;
+    static const wxWindowID ID_STATICTEXT3;
     //*)
 
     static const long ID_MODELS_UNDO;
