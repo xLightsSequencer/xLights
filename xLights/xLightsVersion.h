3c5857c420c6dff1b24f61f4cbcfe0fdde43c007
Daniel Kulp <dan@kulp.com>
2025-01-20 12:36:21 -0500

    Next development iteration

--- xLights/xLightsVersion.h
+++ xLights/xLightsVersion.h
@@ -15,7 +15,7 @@
 #if __has_include("xlights_build_version.h")
 #include "xlights_build_version.h"
 #else
-static const wxString xlights_version_string  = "2025.01";
+static const wxString xlights_version_string  = "2025.01.1";
 #endif
 #ifdef DEBUG
 static const wxString xlights_qualifier       = " DEBUG";
