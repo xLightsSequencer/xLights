6519342d1857b6d16c7ff5de64a82bef0047944a
Daniel Kulp <dan@kulp.com>
2025-01-25 10:40:37 -0500

    Don't pollute the recent sequences list during batch render

--- xLights/SeqFileUtilities.cpp
+++ xLights/SeqFileUtilities.cpp
@@ -576,13 +576,15 @@ void xLightsFrame::OpenSequence(const wxString& passed_filename, ConvertLogDialo
 
 void xLightsFrame::AddToMRU(const std::string& filename)
 {
-    if (mruFiles.Index(filename) != wxNOT_FOUND) {
-        mruFiles.Remove(filename);
-    }
-    if (mruFiles.empty()) {
-        mruFiles.push_back(filename);
-    } else {
-        mruFiles.Insert(filename, 0);
+    if (!_renderMode) {
+        if (mruFiles.Index(filename) != wxNOT_FOUND) {
+            mruFiles.Remove(filename);
+        }
+        if (mruFiles.empty()) {
+            mruFiles.push_back(filename);
+        } else {
+            mruFiles.Insert(filename, 0);
+        }
     }
 }
 
