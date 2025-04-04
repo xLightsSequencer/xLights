64efc9240d8ad3e36789b1e86aebb6f77ff705ad
Daniel Kulp <dan@kulp.com>
2025-04-04 10:51:12 -0400

    Completely remove the bell at render end.  It's obnoxious.

--- xLights/Render.cpp
+++ xLights/Render.cpp
@@ -1290,9 +1290,6 @@ void xLightsFrame::UpdateRenderStatus() {
         }
 
         if (done) {
-            if (!_renderMode && mRendering) {
-                wxBell();
-            }
             for (size_t row = 0; row < rpi->numRows; ++row) {
                 if (rpi->jobs[row]) {
                     delete rpi->jobs[row];
