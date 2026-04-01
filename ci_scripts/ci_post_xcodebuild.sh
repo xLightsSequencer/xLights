c3c33977329d59b4d69f0be0a5b1bb4c783aee0f
Daniel Kulp <dan@kulp.com>
2026-04-01 14:16:31 -0400

    Update generation of WhatToTest file

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -2,11 +2,12 @@
 
 # By default, xcodecloud does --depth 1 so NO history is available.  We
 # need to fetch a longer history and fetch the tags so
-# we can get the list of changes.   200 commits should be enough
+# we can get the list of changes.   400 commits should be enough
 # considering how often we release.
-git fetch --tags -f --deepen 200 origin ${CI_COMMIT}
+git fetch --tags -f --deepen 400 origin ${CI_COMMIT}
+git submodule update --init 
 git status | head -n 1 | tee WhatToTest.en-US.txt
-git log $(git describe --abbrev=0 HEAD^)..HEAD --format="%s (%aN)" | tee -a WhatToTest.en-US.txt
+git log $(git describe --abbrev=0 HEAD^)..HEAD --format="%s (%aN)" | head -n 50 | tee -a WhatToTest.en-US.txt
 
 
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
