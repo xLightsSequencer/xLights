844581a7ffbd4d04be280e3158945cc97f14e139
Daniel Kulp <dan@kulp.com>
2026-04-01 13:15:26 -0400

    Don't log in SpecialOptions.  If Logging hasn't been configured yet, it may cause a crash.

--- xLights/utils/SpecialOptions.cpp
+++ xLights/utils/SpecialOptions.cpp
@@ -34,62 +34,45 @@ std::string SpecialOptions::GetOption(const std::string& option, const std::stri
 
     std::string file = StashShowDir() + GetPathSeparator() + "special.options";
 
-    if (option == "")
-    {
+    if (option == "") {
         __loaded = false;
         __cache.clear();
     }
 
-    if (!FileExists(file))
-    {
-        if (__loaded)
-        {
+    if (!FileExists(file)) {
+        if (__loaded) {
             __loaded = false;
-        }
-        else
-        {
-            spdlog::debug("Special options file not found at " + file);
+        } else {
             __loaded = true;
         }
         __cache.clear();
         return defaultValue;
     }
 
-    if (!__loaded)
-    {
-        spdlog::info("Loading special options from " + file);
+    if (!__loaded) {
         pugi::xml_document doc;
         auto result = doc.load_file(file.c_str());
-        if (result && doc.document_element())
-        {
+        if (result && doc.document_element()) {
             __loaded = true;
-            for (pugi::xml_node n = doc.document_element().first_child(); n; n = n.next_sibling())
-            {
+            for (pugi::xml_node n = doc.document_element().first_child(); n; n = n.next_sibling()) {
                 std::string nodeName = n.name();
                 std::transform(nodeName.begin(), nodeName.end(), nodeName.begin(), ::tolower);
-                if (nodeName == "option")
-                {
+                if (nodeName == "option") {
                     std::string name = Trim(n.attribute("name").as_string());
                     std::string value = n.attribute("value").as_string();
-                    if (name != "")
-                    {
+                    if (name != "") {
                         __cache[name] = value;
-                        spdlog::info("   Option '" + name + "' = '" + value + "'");
                     }
                 }
             }
-        }
-        else
-        {
-            spdlog::error("Unable to load " + file + " invalid xml.");
+        } else {
             return defaultValue;
         }
     }
 
     if (option == "") return defaultValue;
 
-    if (__cache.find(option) == __cache.end())
-    {
+    if (__cache.find(option) == __cache.end()) {
         return defaultValue;
     }
 
