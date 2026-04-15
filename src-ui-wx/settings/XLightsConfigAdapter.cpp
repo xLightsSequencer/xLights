e12c19794b65880d0ab34499fd65cc9a3f180abe
Scott Hanson <scooter_seh@yahoo.com>
2026-04-14 21:08:18 -0400

    add header

--- src-ui-wx/settings/XLightsConfigAdapter.cpp
+++ src-ui-wx/settings/XLightsConfigAdapter.cpp
@@ -1,4 +1,13 @@
-#include "XLightsConfigAdapter.h"
+/***************************************************************
+ * This source files comes from the xLights project
+ * https://www.xlights.org
+ * https://github.com/xLightsSequencer/xLights
+ * See the github commit history for a record of contributing
+ * developers.
+ * Copyright claimed based on commit dates recorded in Github
+ * License: https://github.com/xLightsSequencer/xLights/blob/master/License.txt
+ **************************************************************/
+ #include "XLightsConfigAdapter.h"
 
 #include "settings/XLightsSettings.h"
 #if __has_include(<wx/config.h>)
